5bf3ac8cbc78cc5c91267ef346b466ada2d72084
David Blatcher <dv.bltchr@gmail.com>
2023-01-19 15:00:00 +0000

    generate react ui project

---
+++ apps/newsletters-ui/src/app/App.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <p>Newsletters is running</p>
+    </StyledApp>
+  );
+}
+
+export default App;
---
+++ apps/newsletters-ui/vite.config.ts
@@ -0,0 +1,40 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+
+  plugins: [
+    react(),
+    viteTsConfigPaths({
+      root: '../../',
+    }),
+  ],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [
+  //    viteTsConfigPaths({
+  //      root: '../../',
+  //    }),
+  //  ],
+  // },
+
+  define: {
+    'import.meta.vitest': undefined,
+  },
+  test: {
+    globals: true,
+    cache: {
+      dir: '../../node_modules/.vitest',
+    },
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    includeSource: ['src/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
+});
