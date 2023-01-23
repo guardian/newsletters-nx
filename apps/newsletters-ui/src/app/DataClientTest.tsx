4473a4f8b691f7ae7d859c38a71e7245b3855d87
David Blatcher <dv.bltchr@gmail.com>
2023-01-23 15:37:19 +0000

    run the nx format-check

--- apps/newsletters-ui/src/app/DataClientTest.tsx
+++ apps/newsletters-ui/src/app/DataClientTest.tsx
@@ -1,8 +1,8 @@
 import { newslettersDataClient } from '@newsletters-nx/newsletters-data-client';
 
 export const DataClientTest = () => (
-  <div>
-    <p>the newslettersDataClient function returns:</p>
-    <pre>{newslettersDataClient()}</pre>
-  </div>
+	<div>
+		<p>the newslettersDataClient function returns:</p>
+		<pre>{newslettersDataClient()}</pre>
+	</div>
 );
