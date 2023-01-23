4371660b11f17a78b4394293cf04de37e0a1faed
Charlotte <43961396+cemms1@users.noreply.github.com>
2023-01-23 12:20:11 +0000

    fix linting errors and warnings by adding temp code

--- apps/newsletters-ui/src/app/Api.tsx
+++ apps/newsletters-ui/src/app/Api.tsx
@@ -1,20 +1,36 @@
 import { useEffect, useState } from 'react';
 
-export function Api() {
-  const [response, setResponse] = useState<any | undefined>(undefined);
-
-  useEffect(() => {
-    async function fetchData() {
-      const fetchResponse = await fetch('/api/health');
-
-      setResponse(await fetchResponse.json());
-    }
-    fetchData();
-  }, []);
-  return (
-    <div style={{ background: 'skyblue' }}>
-      <p>Json from API request:</p>
-      <pre>{JSON.stringify(response, null, 2)}</pre>
-    </div>
-  );
+interface HealthData {
+	status: string;
+	message: string;
+	stringFromLib: string;
 }
+
+export const HealthCheck: React.FC = () => {
+	const [response, setResponse] = useState<HealthData>();
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		fetch('/api/health')
+			.then((response: Response) => response.json())
+			.then((data: HealthData) => {
+				setResponse(data);
+				setIsLoading(false);
+			})
+			.catch((error) => {
+				console.error('Error fetching health data:', error);
+				setIsLoading(false);
+			});
+	}, []);
+
+	return isLoading ? (
+		<p>Loading...</p>
+	) : (
+		<div style={{ background: 'skyblue' }}>
+			<p>Json from API request:</p>
+			<pre>{JSON.stringify(response)}</pre>
+		</div>
+	);
+};
+
+export const Api = () => <HealthCheck />;
--- apps/newsletters-ui/src/main.tsx
+++ apps/newsletters-ui/src/main.tsx
@@ -1,16 +1,15 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
-import App from './app/App';
+import { App } from './app/App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+	<StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</StrictMode>,
 );
