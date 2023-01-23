e817173b696765004a55378cf58d57fe74a65343
David Blatcher <dv.bltchr@gmail.com>
2023-01-23 13:05:14 +0000

    install source libraries

--- apps/newsletters-ui/src/app/components/EmotionTest.tsx
+++ apps/newsletters-ui/src/app/components/EmotionTest.tsx
@@ -1,27 +1,36 @@
 import { css } from '@emotion/react';
+import {
+  neutral,
+  brand,
+  news,
+  textSansObjectStyles,
+} from '@guardian/source-foundations';
+import { Container } from '@guardian/source-react-components';
 
 interface Props {
   theme: 'dark' | 'light';
 }
 
 const testStyle = (theme: 'dark' | 'light') => css`
+  ${textSansObjectStyles.medium()};
   padding: 1rem;
   margin-bottom: 1rem;
-  background-color: ${theme === 'dark' ? 'darkred' : 'pink'};
-  color: ${theme === 'dark' ? 'white' : 'black'};
+  background-color: ${theme === 'dark' ? brand[100] : brand[800]};
+  color: ${theme === 'dark' ? neutral[97] : neutral[7]};
 
   b {
-    color: ${theme === 'dark' ? 'skyblue' : 'blue'};
+    color: ${theme === 'dark' ? news[500] : news[200]};
   }
 `;
 
 export const EmotionTest = ({ theme }: Props) => {
   return (
-    <div css={testStyle(theme)}>
+    <Container css={testStyle(theme)}>
+      Container theme: {theme}
       <p>
         This is a paragraph with a <b>part in a B tag</b> and some emotion
         styling.
       </p>
-    </div>
+    </Container>
   );
 };
