ad145160957b79a43270c348b08dc82ca38450c7
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2023-01-24 11:59:42 +0000

    Change case

--- apps/newsletters-ui/src/app/App.tsx
+++ apps/newsletters-ui/src/app/App.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { Api } from './Api';
-import { ButtonContainer } from './Components/ButtonContainer';
+import { ButtonContainer } from './components/ButtonContainer';
 import { DataClientTest } from './DataClientTest';
 
 const StyledApp = styled.div`
