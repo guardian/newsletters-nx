6b421251c8cc77c868cd6257cbb3badc3549eb73
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2023-01-26 08:47:51 +0000

    Display list of newsletters

--- apps/newsletters-ui/src/app/components/NewslettersTable.tsx
+++ apps/newsletters-ui/src/app/components/NewslettersTable.tsx
@@ -9,31 +9,14 @@ interface Props {
 
 export const NewslettersTable = ({newsletters}: Props) => {
   const data = useMemo<object[]>(
-    () => [
-      {
-        col1: 'Hello',
-        col2: 'World',
-      },
-      {
-        col1: 'react-table',
-        col2: 'rocks',
-      },
-      {
-        col1: 'whatever',
-        col2: 'you want',
-      },
-    ],
-    [],
+    () => newsletters,
+    [newsletters],
   );
   const columns = useMemo<Column[]>(
     () => [
       {
-        Header: 'Column 1',
-        accessor: 'col1',
-      },
-      {
-        Header: 'Column 2',
-        accessor: 'col2',
+        Header: 'Identity Name',
+        accessor: 'identityName',
       },
     ],
     [],
