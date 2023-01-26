125f6e8ce35dd88cd20865e6378d6bc9227d11da
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2023-01-26 08:42:26 +0000

    Pass in newsletter data

--- apps/newsletters-ui/src/app/components/NewslettersListView.tsx
+++ apps/newsletters-ui/src/app/components/NewslettersListView.tsx
@@ -23,6 +23,5 @@ export const NewsletterListView = () => {
 	// 		</ul>
 	// 	</nav>
 	// );
-	// return <NewslettersTable newsletters={newsLetters} />
-	return <NewslettersTable />
+	return <NewslettersTable newsletters={newsLetters} />
 };
--- apps/newsletters-ui/src/app/components/NewslettersTable.tsx
+++ apps/newsletters-ui/src/app/components/NewslettersTable.tsx
@@ -1,8 +1,13 @@
 import { useMemo } from 'react';
 import type { Column } from 'react-table';
+import type { Newsletter } from '@newsletters-nx/newsletters-data-client';
 import { Table } from './Table';
 
-export const NewslettersTable = () => {
+interface Props {
+	newsletters: Newsletter[];
+}
+
+export const NewslettersTable = ({newsletters}: Props) => {
   const data = useMemo<object[]>(
     () => [
       {
