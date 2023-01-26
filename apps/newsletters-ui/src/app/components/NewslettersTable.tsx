8e733a928a2dd796adef916d9ab7d26f184d89f2
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2023-01-26 09:53:35 +0000

    Format link in column definition

--- apps/newsletters-ui/src/app/components/NewslettersTable.tsx
+++ apps/newsletters-ui/src/app/components/NewslettersTable.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { Link } from 'react-router-dom';
 import type { Column } from 'react-table';
 import type { Newsletter } from '@newsletters-nx/newsletters-data-client';
 import { Table } from './Table';
@@ -17,7 +18,7 @@ export const NewslettersTable = ({newsletters}: Props) => {
       {
         Header: 'Newsletter ID',
         accessor: 'identityName',
-				
+				Cell: ({ cell: { value } }) => <Link to={`/newsletters/${value as string}`}>{value}</Link>
       },
       {
         Header: 'Newsletter Name',
--- apps/newsletters-ui/src/app/components/Table.tsx
+++ apps/newsletters-ui/src/app/components/Table.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { css } from '@emotion/react';
 import { useTable } from 'react-table';
 import type { Cell, Column } from 'react-table';
 
@@ -11,8 +11,17 @@ export const Table = ({ data, columns }: Props) => {
 	const tableInstance = useTable({ columns, data });
 	const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
 		tableInstance;
+	const tableStyle = css`
+		border-collapse: collapse;
+		th,
+		td {
+			border: 1px solid #dddddd;
+			padding: 8px;
+			text-align: left;
+		}
+	`;
 	return (
-		<table {...getTableProps()}>
+		<table {...getTableProps()} css={tableStyle}>
 			<thead>
 				{headerGroups.map((headerGroup) => (
 					<tr {...headerGroup.getFooterGroupProps()}>
@@ -28,17 +37,7 @@ export const Table = ({ data, columns }: Props) => {
 					return (
 						<tr {...row.getRowProps()}>
 							{row.cells.map((cell: Cell, index) => {
-								return (
-									<td {...cell.getCellProps()}>
-										{index === 0 ? (
-											<Link to={`/newsletters/${cell.value as string}`}>
-												{cell.render('Cell')}
-											</Link>
-										) : (
-											cell.render('Cell')
-										)}
-									</td>
-								);
+								return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
 							})}
 						</tr>
 					);
