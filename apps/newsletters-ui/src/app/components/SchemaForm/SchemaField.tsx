c0036b787d2152ea68934b5f9888b5839d9dd209
David Blatcher <dv.bltchr@gmail.com>
2023-01-27 12:54:46 +0000

    refactor schemaField

--- apps/newsletters-ui/src/app/components/SchemaForm/SchemaField.tsx
+++ apps/newsletters-ui/src/app/components/SchemaForm/SchemaField.tsx
@@ -1,5 +1,8 @@
-import { Checkbox, TextInput } from '@guardian/source-react-components';
-import type { ReactNode } from 'react';
+import {
+	Checkbox,
+	InlineError,
+	TextInput,
+} from '@guardian/source-react-components';
 import type { z } from 'zod';
 import { SchemaNumber } from './SchemaNumber';
 import { SchemaSelect } from './SchemaSelect';
@@ -15,6 +18,22 @@ interface SchemaFieldProps<T> {
 	numberInputSettings?: NumberInputSettings;
 }
 
+const fieldValueAsSting = (field: FieldDef): string => {
+	switch (typeof field.value) {
+		case 'string':
+		case 'boolean':
+		case 'number':
+			return field.value.toString();
+		case 'object':
+			if (Array.isArray(field.value)) {
+				return 'ARRAY';
+			}
+			return field.value ? field.value.toString() : 'NULL';
+		default:
+			return '';
+	}
+};
+
 export function SchemaField<T extends z.ZodRawShape>({
 	field,
 	change,
@@ -24,99 +43,77 @@ export function SchemaField<T extends z.ZodRawShape>({
 	numberInputSettings = {},
 }: SchemaFieldProps<T>) {
 	const { key, type, value } = field;
-	const isSupported = [
-		'ZodString',
-		'ZodBoolean',
-		'ZodNumber',
-		'ZodEnum',
-	].includes(type);
-	if (!isSupported && !showUnsupported) {
-		return null;
+
+	if (
+		type === 'ZodString' &&
+		(typeof value === 'string' || typeof value === 'undefined')
+	) {
+		if (options) {
+			return <SchemaSelect field={field} change={change} options={options} />;
+		}
+
+		return (
+			<TextInput
+				label={key}
+				value={value}
+				type={stringInputType}
+				onChange={(event) => {
+					change(eventToString(event), field);
+				}}
+				required={!field.optional}
+			/>
+		);
 	}
 
-	let safeValue: FieldValue;
-	switch (typeof value) {
-		case 'string':
-		case 'boolean':
-		case 'number':
-			// eslint-disable-next-line prefer-const -- switch
-			safeValue = value;
+	if (
+		type === 'ZodBoolean' &&
+		(typeof value === 'boolean' || typeof value === 'undefined')
+	) {
+		return (
+			<Checkbox
+				label={key}
+				checked={!!value}
+				onChange={(event) => {
+					change(eventToBoolean(event), field);
+				}}
+			/>
+		);
 	}
 
-	function buildInput(): ReactNode | null {
-		if (
-			type === 'ZodString' &&
-			(typeof value === 'string' || typeof value === 'undefined')
-		) {
-			if (options) {
-				return <SchemaSelect field={field} change={change} options={options} />;
-			}
-
+	if (type === 'ZodNumber') {
+		if (typeof value === 'number' || typeof value === 'undefined') {
 			return (
-				<TextInput
-					label={key}
-					value={value}
-					type={stringInputType}
-					onChange={(event) => {
-						change(eventToString(event), field);
-					}}
-					required={!field.optional}
+				<SchemaNumber
+					numberValue={value}
+					field={field}
+					change={change}
+					numberInputSettings={numberInputSettings}
 				/>
 			);
 		}
+	}
 
-		if (
-			type === 'ZodBoolean' &&
-			(typeof value === 'boolean' || typeof value === 'undefined')
-		) {
+	if (type === 'ZodEnum' && field.enumOptions) {
+		if (typeof value === 'string') {
 			return (
-				<Checkbox
-					label={key}
-					checked={!!value}
-					onChange={(event) => {
-						change(eventToBoolean(event), field);
-					}}
+				<SchemaSelect
+					field={field}
+					change={change}
+					options={field.enumOptions}
 				/>
 			);
 		}
-
-		if (type === 'ZodNumber') {
-			if (typeof value === 'number' || typeof value === 'undefined') {
-				return (
-					<SchemaNumber
-						numberValue={value}
-						field={field}
-						change={change}
-						numberInputSettings={numberInputSettings}
-					/>
-				);
-			}
-		}
-
-		if (type === 'ZodEnum' && field.enumOptions) {
-			if (typeof value === 'string') {
-				return (
-					<SchemaSelect
-						field={field}
-						change={change}
-						options={field.enumOptions}
-					/>
-				);
-			}
-		}
-
-		if (showUnsupported) {
-			return (
-				<div key={key}>
-					<b>UNSUPPORTED | </b>
-					{key} |{type}
-					<b>{safeValue?.toString()}</b>
-				</div>
-			);
-		}
-
-		return null;
 	}
 
-	return buildInput();
+	if (showUnsupported) {
+		return (
+			<InlineError>
+				<b>UNSUPPORTED FIELD TYPE [{type}] : </b>
+				{key}
+				<b>{fieldValueAsSting(field)}</b>
+			</InlineError>
+		);
+	}
+
+	return null;
 }
