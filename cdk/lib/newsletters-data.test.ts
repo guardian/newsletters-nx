30d5a5e1840156f0a1c0d7d64cea517e2c734262
Charlotte <43961396+cemms1@users.noreply.github.com>
2023-01-31 17:52:39 +0000

    test: change test name

--- cdk/lib/newsletters-data.test.ts
+++ cdk/lib/newsletters-data.test.ts
@@ -3,7 +3,7 @@ import { Template } from 'aws-cdk-lib/assertions';
 import { newslettersProps } from '../bin/cdk';
 import { NewslettersData } from './newsletters-data';
 
-describe('The Newsletters API stack', () => {
+describe('The Newsletters Data stack', () => {
 	it('matches the snapshot', () => {
 		const app = new App();
 		const stack = new NewslettersData(app, 'NewslettersData-TEST', {
