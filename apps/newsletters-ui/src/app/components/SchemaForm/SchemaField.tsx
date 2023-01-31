5d8e188a7d5b8d6ca5842710824cb6c45bb7f7c4
David Blatcher <dv.bltchr@gmail.com>
2023-01-31 09:15:43 +0000

    use switch block

--- apps/newsletters-ui/src/app/components/SchemaForm/SchemaField.tsx
+++ apps/newsletters-ui/src/app/components/SchemaForm/SchemaField.tsx
@@ -29,6 +29,13 @@ const fieldValueAsDisplayString = (field: FieldDef): string => {
 	}
 };
 
+const WrongTypeMessage = (props: { field: FieldDef }) => (
+	<div>
+		wrong value type ({fieldValueAsDisplayString(props.field)}) for{' '}
+		{props.field.type}
+	</div>
+);
+
 export function SchemaField<T extends z.ZodRawShape>({
 	field,
 	change,
@@ -40,58 +47,60 @@ export function SchemaField<T extends z.ZodRawShape>({
 }: SchemaFieldProps<T>) {
 	const { key, type, value } = field;
 
-	if (
-		type === 'ZodString' &&
-		(typeof value === 'string' || typeof value === 'undefined')
-	) {
-		if (options) {
+	switch (type) {
+		case 'ZodString':
+			if (typeof value !== 'string' && typeof value !== 'undefined') {
+				return <WrongTypeMessage field={field} />;
+			}
+
+			if (options) {
+				return (
+					<SelectInput
+						label={field.key}
+						value={value}
+						optional={field.optional}
+						inputHandler={(newValue) => {
+							change(newValue, field);
+						}}
+						error={validationWarning}
+						options={options}
+					/>
+				);
+			}
+
 			return (
-				<SelectInput
-					label={field.key}
-					value={value}
+				<StringInput
+					label={key}
+					value={value ?? ''}
+					type={stringInputType}
 					optional={field.optional}
 					inputHandler={(newValue) => {
 						change(newValue, field);
 					}}
 					error={validationWarning}
-					options={options}
 				/>
 			);
-		}
 
-		return (
-			<StringInput
-				label={key}
-				value={value ?? ''}
-				type={stringInputType}
-				optional={field.optional}
-				inputHandler={(newValue) => {
-					change(newValue, field);
-				}}
-				error={validationWarning}
-			/>
-		);
-	}
+		case 'ZodBoolean':
+			if (typeof value !== 'boolean' && typeof value !== 'undefined') {
+				return <WrongTypeMessage field={field} />;
+			}
+			return (
+				<BooleanInput
+					label={key}
+					value={value ?? false}
+					optional={field.optional}
+					inputHandler={(newValue) => {
+						change(newValue, field);
+					}}
+					error={validationWarning}
+				/>
+			);
 
-	if (
-		type === 'ZodBoolean' &&
-		(typeof value === 'boolean' || typeof value === 'undefined')
-	) {
-		return (
-			<BooleanInput
-				label={key}
-				value={value ?? false}
-				optional={field.optional}
-				inputHandler={(newValue) => {
-					change(newValue, field);
-				}}
-				error={validationWarning}
-			/>
-		);
-	}
-
-	if (type === 'ZodNumber') {
-		if (typeof value === 'number' || typeof value === 'undefined') {
+		case 'ZodNumber':
+			if (typeof value !== 'number' && typeof value !== 'undefined') {
+				return <WrongTypeMessage field={field} />;
+			}
 			return (
 				<SchemaNumber
 					numberValue={value}
@@ -101,11 +110,11 @@ export function SchemaField<T extends z.ZodRawShape>({
 					validationWarning={validationWarning}
 				/>
 			);
-		}
-	}
 
-	if (type === 'ZodEnum' && field.enumOptions) {
-		if (typeof value === 'string') {
+		case 'ZodEnum':
+			if (typeof value !== 'string' && typeof value !== 'undefined') {
+				return <WrongTypeMessage field={field} />;
+			}
 			return (
 				<SelectInput
 					label={field.key}
@@ -115,21 +124,21 @@ export function SchemaField<T extends z.ZodRawShape>({
 						change(newValue, field);
 					}}
 					error={validationWarning}
-					options={field.enumOptions}
+					options={field.enumOptions ?? []}
 				/>
 			);
-		}
-	}
 
-	if (showUnsupported) {
-		return (
-			<div>
-				<b>UNSUPPORTED FIELD TYPE [{type}] : </b>
-				{key}
-				<b>{fieldValueAsDisplayString(field)}</b>
-			</div>
-		);
-	}
+		default:
+			if (showUnsupported) {
+				return (
+					<div>
+						<b>UNSUPPORTED FIELD TYPE [{type}] : </b>
+						{key}
+						<b>{fieldValueAsDisplayString(field)}</b>
+					</div>
+				);
+			}
 
-	return null;
+			return null;
+	}
 }
