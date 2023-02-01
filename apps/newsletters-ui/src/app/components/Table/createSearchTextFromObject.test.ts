c07a92ae5f4c81b7287ce51861875786700a88c1
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2023-02-01 09:18:26 +0000

    WIP

--- apps/newsletters-ui/src/app/components/Table/createSearchTextFromObject.test.ts
+++ apps/newsletters-ui/src/app/components/Table/createSearchTextFromObject.test.ts
@@ -1,28 +1,28 @@
-import { filterObjects } from './createSearchTextFromObject';
+// import { filterObjects } from './createSearchTextFromObject';
 
-describe('filterObjects', () => {
-  it('filters users by name', () => {
-    const filteredObjects = filterObjects({ name: 'Jane' });
-    expect(filteredObjects).toEqual([{ name: 'Jane', age: 25, city: 'Los Angeles' }]);
-  });
+// describe('filterObjects', () => {
+  // it('filters users by name', () => {
+  //   const filteredObjects = filterObjects({ name: 'Jane' });
+  //   expect(filteredObjects).toEqual([{ name: 'Jane', age: 25, city: 'Los Angeles' }]);
+  // });
 
-  it('filters users by age', () => {
-    const filteredObjects = filterObjects({ age: 35 });
-    expect(filteredObjects).toEqual([{ name: 'Jim', age: 35, city: 'Chicago' }]);
-  });
+  // it('filters users by age', () => {
+  //   const filteredObjects = filterObjects({ age: 35 });
+  //   expect(filteredObjects).toEqual([{ name: 'Jim', age: 35, city: 'Chicago' }]);
+  // });
 
-  it('filters users by city', () => {
-    const filteredObjects = filterObjects({ city: 'New York' });
-    expect(filteredObjects).toEqual([{ name: 'John', age: 30, city: 'New York' }]);
-  });
+  // it('filters users by city', () => {
+  //   const filteredObjects = filterObjects({ city: 'New York' });
+  //   expect(filteredObjects).toEqual([{ name: 'John', age: 30, city: 'New York' }]);
+  // });
 
-  it('filters users by multiple criteria', () => {
-    const filteredObjects = filterObjects({ name: 'Jane', city: 'Los Angeles' });
-    expect(filteredObjects).toEqual([{ name: 'Jane', age: 25, city: 'Los Angeles' }]);
-  });
+  // it('filters users by multiple criteria', () => {
+  //   const filteredObjects = filterObjects({ name: 'Jane', city: 'Los Angeles' });
+  //   expect(filteredObjects).toEqual([{ name: 'Jane', age: 25, city: 'Los Angeles' }]);
+  // });
 
-  it('returns no data where none found', () => {
-    const filteredObjects = filterObjects({ name: 'Jane', city: 'New York' });
-    expect(filteredObjects).toEqual([]);
-  });
-});
+  // it('returns no data where none found', () => {
+  //   const filteredObjects = filterObjects({ name: 'Jane', city: 'New York' });
+  //   expect(filteredObjects).toEqual([]);
+  // });
+// });
