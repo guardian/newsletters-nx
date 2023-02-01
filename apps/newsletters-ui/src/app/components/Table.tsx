747fffb3e1d9bdf8b0fc2638e60587c7bb4cc6b8
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2023-02-01 08:43:11 +0000

    Filter on hardcoded name

--- apps/newsletters-ui/src/app/components/Table.tsx
+++ apps/newsletters-ui/src/app/components/Table.tsx
@@ -1,19 +1,32 @@
 import { css } from '@emotion/react';
-import type { Cell, Column } from 'react-table';
+import { useEffect, useState } from 'react';
+import type { Cell, Column, TableOptions } from 'react-table';
 import { useSortBy, useTable } from 'react-table';
 
+interface TableData extends Object {
+	name: string;
+}
 interface Props {
-	data: object[];
+	data: TableData[];
 	columns: Column[];
 	defaultSortId?: string;
 }
 
 export const Table = ({ data, columns, defaultSortId }: Props) => {
+	const [filterText, setFilterText] = useState('');
+	const [filteredData, setFilteredData] = useState<TableData[]>([]);
+	useEffect(() => {
+		setFilteredData(
+			data.filter((d: { name: string }) =>
+				d.name.toLowerCase().includes(filterText.toLowerCase())
+			),
+		)
+	}, [data, filterText]);
 	const initialState = defaultSortId
 		? { sortBy: [{ id: defaultSortId, desc: false }] }
 		: {};
 	const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-		useTable({ columns, data, initialState }, useSortBy);
+		useTable({ columns, data: filteredData, initialState }, useSortBy);
 
 	const tableStyle = css`
 		border-collapse: collapse;
@@ -25,33 +38,47 @@ export const Table = ({ data, columns, defaultSortId }: Props) => {
 		}
 	`;
 	return (
-		<table {...getTableProps()} css={tableStyle}>
-			<thead>
-				{headerGroups.map((headerGroup) => (
-					<tr {...headerGroup.getHeaderGroupProps()}>
-						{headerGroup.headers.map((column) => (
-							<th {...column.getHeaderProps(column.getSortByToggleProps())}>
-								{column.render('Header')}
-								<span>
-									{column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
-								</span>
-							</th>
-						))}
-					</tr>
-				))}
-			</thead>
-			<tbody {...getTableBodyProps()}>
-				{rows.map((row) => {
-					prepareRow(row);
-					return (
-						<tr {...row.getRowProps()}>
-							{row.cells.map((cell: Cell, index) => {
-								return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-							})}
+		<>
+			<input
+				type="text"
+				placeholder="Filter data by name"
+				value={filterText}
+				onChange={(e) => setFilterText(e.target.value)}
+			/>
+			<table {...getTableProps()} css={tableStyle}>
+				<thead>
+					{headerGroups.map((headerGroup) => (
+						<tr {...headerGroup.getHeaderGroupProps()}>
+							{headerGroup.headers.map((column) => (
+								<th {...column.getHeaderProps(column.getSortByToggleProps())}>
+									{column.render('Header')}
+									<span>
+										{column.isSorted
+											? column.isSortedDesc
+												? ' 🔽'
+												: ' 🔼'
+											: ''}
+									</span>
+								</th>
+							))}
 						</tr>
-					);
-				})}
-			</tbody>
-		</table>
+					))}
+				</thead>
+				<tbody {...getTableBodyProps()}>
+					{rows.map((row) => {
+						prepareRow(row);
+						return (
+							<tr {...row.getRowProps()}>
+								{row.cells.map((cell: Cell, index) => {
+									return (
+										<td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+									);
+								})}
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</>
 	);
 };
