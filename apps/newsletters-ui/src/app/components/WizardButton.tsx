b25feadec262f72e0f23d3d66ef9a5ff66c67025
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2023-02-08 14:31:11 +0000

    Linting

--- apps/newsletters-ui/src/app/components/WizardButton.tsx
+++ apps/newsletters-ui/src/app/components/WizardButton.tsx
@@ -9,10 +9,7 @@ export function WizardButton({
 }: WizardButtonProps) {
 	const { background, border } = WIZARD_BUTTON_TYPES[buttonType];
 	return (
-		<StyledWizardButton
-			background={background}
-			border={border}
-		>
+		<StyledWizardButton background={background} border={border}>
 			{label}
 		</StyledWizardButton>
 	);
