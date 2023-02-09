568bb82e4c74bba6263a7562bf06853f7405856f
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2023-02-09 15:00:24 +0000

    Built user interface for wizard and created dummy API endpoint (#21)
    * Install react-markdown
    
    * Basic markdown components
    
    * Add key for wizard buttons
    
    * Correct key for wizard buttons
    
    * Change font
    
    * Create Button component
    
    * Linting
    
    * Refactor
    
    * Refactored routes into a routes folder.
    
    * WIP Not compiling
    
    * Refactor
    
    * Linting
    
    * Corrections
    
    * Linting
    
    * Create task to shortcut cmd + shift + b for npm run dev
    
    * Correct onClick
    
    * Linting
    
    * Added typedoc comments
    
    * Rename state params
    
    * url changed to lowercase
    
    * Linting
    
    * Update lock file
    
    ---------
    
    Co-authored-by: Idris Elliott <idrise@davidjelliott.com>

---
+++ apps/newsletters-ui/src/app/components/Wizard.tsx
@@ -0,0 +1,95 @@
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import type { WIZARD_BUTTON_TYPES } from '@newsletters-nx/newsletters-data-client';
+import { WizardButton } from './WizardButton';
+
+/**
+ * Interface for a button displayed in the wizard.
+ */
+export interface WizardButton {
+	/** Unique identifier for the button. */
+	id: string;
+	/** Label displayed on the button. */
+	label: string;
+	/** Type of the button, mapped to a specific background and border color. */
+	buttonType: keyof typeof WIZARD_BUTTON_TYPES;
+}
+
+/**
+ * Interface for the response received from the server for a single step in the wizard.
+ */
+interface CurrentStepRouteResponse {
+	/** Markdown content to display for the current step. */
+	markdownToDisplay: string;
+	/** Unique identifier for the current step. */
+	currentStepId: string;
+	/** Buttons to display for the current step. */
+	buttons: Array<{
+		/** Label displayed on the button. */
+		label: string;
+		/** Type of the button, mapped to a specific background and border color. */
+		buttonType: keyof typeof WIZARD_BUTTON_TYPES;
+		/** Unique identifier for the button. */
+		id: string;
+	}>;
+}
+
+/**
+ * Interface for the props passed to the `Wizard` component.
+ */
+export interface WizardProps {
+	/** Markdown content to display for the current step. */
+	markdown: string;
+	/** Unique identifier for the current step. */
+	stepName: string;
+	/** Buttons to display for the current step. */
+	wizardButtons: WizardButton[];
+}
+
+/**
+ * Component that displays a single step in a wizard, including markdown content and buttons.
+ */
+export const Wizard: React.FC<WizardProps> = ({
+	markdown,
+	stepName,
+	wizardButtons,
+}) => {
+	const firstPage: CurrentStepRouteResponse = {
+		buttons: wizardButtons,
+		currentStepId: stepName,
+		markdownToDisplay: markdown,
+	};
+	const [wizardStep, setWizardStep] = useState<
+		CurrentStepRouteResponse | undefined
+	>(firstPage);
+
+	const handleButtonClick = (id: string) => () => {
+		setWizardStep(undefined);
+		fetch(`/api/v1/currentstep`)
+			.then((response) => response.json())
+			.then((data: CurrentStepRouteResponse) => {
+				setWizardStep(data as unknown as CurrentStepRouteResponse);
+			})
+			.catch((error: unknown /* FIXME! */) => {
+				console.error('Error invoking next step of wizard:', error);
+			});
+	};
+
+	if (wizardStep === undefined) {
+		return <p>'loading'</p>;
+	}
+	return (
+		<div className="markdown-block">
+			<ReactMarkdown>{wizardStep.markdownToDisplay}</ReactMarkdown>
+			{wizardStep.buttons.map((button) => (
+				<WizardButton
+					id={button.id}
+					label={button.label}
+					buttonType={button.buttonType}
+					onClick={handleButtonClick(button.id)}
+					key={stepName + button.label}
+				/>
+			))}
+		</div>
+	);
+};
--- apps/newsletters-ui/src/app/styles.ts
+++ apps/newsletters-ui/src/app/styles.ts
@@ -1,11 +1,12 @@
 import { css } from '@emotion/react';
+import { space } from '@guardian/source-foundations';
 
 export const tableStyle = css`
 	border-collapse: collapse;
 	th,
 	td {
 		border: 1px solid #dddddd;
-		padding: 8px;
+		padding: ${space[2]}px;
 		text-align: left;
 	}
 `;
