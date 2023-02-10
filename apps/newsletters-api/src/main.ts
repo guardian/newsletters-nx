6ce06d90319976b0ee6525db24e0bd84720e3b62
akash1810 <akash1810@users.noreply.github.com>
2023-02-10 10:34:05 +0000

    fix: Explicitly set the host to `0.0.0.0`
    This ensures the load balancer can reach the locally running server,
    and thus pass the healthcheck.
    
    Co-authored-by: Julia <julia.branke@guardian.co.uk>
    Co-authored-by: Robert <robert.kenny@guardian.co.uk>
    Co-authored-by: Natasha <natasha.thrale@guardian.co.uk>

--- apps/newsletters-api/src/main.ts
+++ apps/newsletters-api/src/main.ts
@@ -44,8 +44,22 @@ app.get<{ Params: { newsletterId: string } }>(
 );
 const start = async () => {
 	try {
-		console.log('Starting newsletters-api server on http://localhost:3000');
-		await app.listen({ port: 3000 });
+		const options = {
+			port: 3000,
+
+			/**
+			 * 0.0.0.0 so that we listen on every network interface.
+			 * This is essential for running the app within AWS.
+			 *
+			 * See:
+			 *   - https://www.fastify.io/docs/latest/Reference/Server/#listen
+			 * 	 - https://serverfault.com/questions/78048/whats-the-difference-between-ip-address-0-0-0-0-and-127-0-0-1
+			 */
+			host: '0.0.0.0'
+		}
+
+		console.log(`Starting newsletters-api server on http://${options.host}:${options.port}`);
+		await app.listen(options);
 	} catch (err) {
 		// Errors are logged here
 		console.error(err);
