aa4727d9907e7ab8934dc9facd6a13d769b5d995
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2023-02-15 16:30:16 +0000

    Add tests

--- libs/state-machine/src/lib/stateMachineButtonPressed.spec.ts
+++ libs/state-machine/src/lib/stateMachineButtonPressed.spec.ts
@@ -1,10 +1,14 @@
 import { stateMachineButtonPressed } from './stateMachineButtonPressed';
 import type { WizardStatic, WizardStep } from './types';
 
-const mockStep: WizardStep = {
+const initialStep: WizardStep = {
 	currentStepId: 'step1',
 	errorMessage: '',
 };
+let mockStep: WizardStep;
+beforeEach(() => {
+	mockStep = Object.assign({}, initialStep);
+});
 
 const mockStatic: WizardStatic = {
 	step1: {
@@ -14,17 +18,17 @@ const mockStatic: WizardStatic = {
 				buttonType: 'RED',
 				label: 'Next',
 				stepToMoveTo: 'step2',
+				onAfterStepStartValidate: jest.fn(),
 				executeStep: jest.fn(),
 				onBeforeStepChangeValidate: jest.fn(),
-				onAfterStepStartValidate: jest.fn(),
 			},
 			cancel: {
 				buttonType: 'RED',
 				label: 'Cancel',
-				stepToMoveTo: 'step1',
+				stepToMoveTo: 'exit',
+				onAfterStepStartValidate: jest.fn(),
 				executeStep: jest.fn(),
 				onBeforeStepChangeValidate: jest.fn(),
-				onAfterStepStartValidate: jest.fn(),
 			},
 		},
 	},
@@ -35,19 +39,23 @@ const mockStatic: WizardStatic = {
 				buttonType: 'RED',
 				label: 'Previous',
 				stepToMoveTo: 'step1',
+				onAfterStepStartValidate: jest.fn(),
 				executeStep: jest.fn(),
 				onBeforeStepChangeValidate: jest.fn(),
-				onAfterStepStartValidate: jest.fn(),
 			},
 			finish: {
-				buttonType: 'RED',
+				buttonType: 'GREEN',
 				label: 'Finish',
-				stepToMoveTo: 'step1',
-				executeStep: jest.fn(),
+				stepToMoveTo: 'exit',
 				onBeforeStepChangeValidate: jest.fn(),
+				executeStep: jest.fn(),
 				onAfterStepStartValidate: jest.fn(),
 			},
 		},
+	},
+	exit: {
+		markdownToDisplay: 'Exit',
+		buttons: {},
 	},
 };
 
@@ -59,59 +67,92 @@ describe('stateMachineButtonPressed', () => {
 	});
 });
 
-// it('should execute step and move to next step if next button is pressed', async () => {
-// 	const executeStepMock = jest.fn().mockResolvedValue(undefined);
-// 	let executeStep = mockStatic['step1']?.buttons['next']?.executeStep;
-// 	if (executeStep) {
-// 		executeStep = executeStepMock;
-// 		const newState = await stateMachineButtonPressed(
-// 			'next',
-// 			mockStep,
-// 			mockStatic,
-// 		);
-// 		expect(executeStepMock).toHaveBeenCalledWith(
-// 			mockStep,
-// 			mockStatic['step1']?.buttons['next'],
-// 		);
-// 		expect(newState.currentStepId).toEqual('step2');
-// 	}
+it('should execute step and move to next step if next button is pressed', async () => {
+	const executeStepMock = jest.fn().mockResolvedValue(undefined);
+	const nextButton = mockStatic['step1']?.buttons['next'];
+	const executeStep = nextButton?.executeStep;
+	if (nextButton && executeStep) {
+		nextButton.executeStep = executeStepMock;
+		const newState = await stateMachineButtonPressed(
+			'next',
+			mockStep,
+			mockStatic,
+		);
+		expect(executeStepMock).toHaveBeenCalledWith(mockStep, mockStatic['step1']);
+		expect(newState.currentStepId).toEqual('step2');
+	}
+});
 
-// const nextButton = mockStatic.step1.buttons.next;
-// nextButton.executeStep.mockReturnValue(undefined);
-// const result = await stateMachineButtonPressed(mockState, 'next', mockStatic);
-// expect(result?.currentStepId).toEqual('step2');
-// expect(nextButton.executeStep).toHaveBeenCalled();
-// });
+it('should validate before step change and return error message if there is validation error', async () => {
+	const onBeforeStepChangeValidateMock = jest
+		.fn()
+		.mockReturnValue('Validation error');
+	const nextButton = mockStatic['step1']?.buttons['next'];
+	const onBeforeStepChangeValidate = nextButton?.onBeforeStepChangeValidate;
+	if (nextButton && onBeforeStepChangeValidate) {
+		nextButton.onBeforeStepChangeValidate = onBeforeStepChangeValidateMock;
+		const result = await stateMachineButtonPressed(
+			'next',
+			mockStep,
+			mockStatic,
+		);
+		expect(onBeforeStepChangeValidateMock).toHaveBeenCalledWith(
+			mockStep,
+			mockStatic['step1'],
+		);
+		expect(result.currentStepId).toEqual('step1');
+		expect(result.errorMessage).toEqual('Validation error');
+	}
+});
 
-// it('should validate before step change and return error message if there is validation error', async () => {
-// const nextButton = mockStatic.step1.buttons.next;
-// nextButton.onBeforeStepChangeValidate.mockReturnValue('Validation error');
-// const result = await stateMachineButtonPressed(mockState, 'next', mockStatic);
-// expect(result?.currentStepId).toEqual('step1');
-// expect(nextButton.onBeforeStepChangeValidate).toHaveBeenCalled();
-// expect(result?.errorMessage).toEqual('Validation error');
-// });
+it('should validate after step start and return error message if there is validation error', async () => {
+	const onAfterStepStartValidateMock = jest
+		.fn()
+		.mockReturnValue('Validation error');
+	const nextButton = mockStatic['step1']?.buttons['next'];
+	const onAfterStepStartValidate = nextButton?.onAfterStepStartValidate;
+	if (nextButton && onAfterStepStartValidate) {
+		nextButton.onAfterStepStartValidate = onAfterStepStartValidateMock;
+		const result = await stateMachineButtonPressed(
+			'next',
+			mockStep,
+			mockStatic,
+		);
+		expect(onAfterStepStartValidateMock).toHaveBeenCalledWith(mockStep);
+		expect(result.currentStepId).toEqual('step1');
+		expect(result.errorMessage).toEqual('Validation error');
+	}
+});
 
-// it('should validate after step start and return error message if there is validation error', async () => {
-// const nextButton = mockStatic.step1.buttons.next;
-// nextButton.onAfterStepStartValidate.mockReturnValue('Validation error');
-// const result = await stateMachineButtonPressed(mockState, 'next', mockStatic);
-// expect(result?.currentStepId).toEqual('step2');
-// expect(nextButton.onAfterStepStartValidate).toHaveBeenCalled();
-// expect(result?.errorMessage).toEqual('Validation error');
-// });
+it('should fail to execute step and return error message if there is validation error', async () => {
+	const executeStepMock = jest.fn().mockReturnValue('Validation error');
+	const nextButton = mockStatic['step1']?.buttons['next'];
+	const executeStep = nextButton?.executeStep;
+	if (nextButton && executeStep) {
+		nextButton.executeStep = executeStepMock;
+		const result = await stateMachineButtonPressed(
+			'next',
+			mockStep,
+			mockStatic,
+		);
+		expect(executeStepMock).toHaveBeenCalled;
+		expect(result.currentStepId).toEqual('step1');
+		expect(result.errorMessage).toEqual('Validation error');
+	}
+});
 
-// it('should execute step and return error message if there is validation error', async () => {
-// const nextButton = mockStatic.step1.buttons.next;
-// nextButton.executeStep.mockReturnValue('Validation error');
-// const result = await stateMachineButtonPressed(mockState, 'next', mockStatic);
-// expect(result?.currentStepId).toEqual('step1');
-// expect(nextButton.executeStep).toHaveBeenCalled();
-// expect(result?.errorMessage).toEqual('Validation error');
-// });
-
-// it('should move to previous step if prev button is pressed', async () => {
-// const prevButton = mockStatic.step2.buttons.prev;
-// const result = await stateMachineButtonPressed({...mockState, currentStepId: 'step2'}, 'prev', mockStatic);
-// expect(result?.currentStepId).toEqual('step1');
-// });
+it('should move to previous step if prev button is pressed', async () => {
+	const executeStepMock = jest.fn().mockResolvedValue(undefined);
+	const backButton = mockStatic['step1']?.buttons['back'];
+	const executeStep = backButton?.executeStep;
+	if (backButton && executeStep) {
+		backButton.executeStep = executeStepMock;
+		const newState = await stateMachineButtonPressed(
+			'back',
+			mockStep,
+			mockStatic,
+		);
+		expect(executeStepMock).toHaveBeenCalledWith(mockStep, mockStatic['step1']);
+		expect(newState.currentStepId).toEqual('exit');
+	}
+});
