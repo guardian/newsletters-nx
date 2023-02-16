5187126116c530ed1c3ea73093b7cd34ab9d91ea
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2023-02-16 14:23:03 +0000

    Refactor workflow

--- apps/newsletters-api/src/app/routes/newsletterWorkflow.ts
+++ apps/newsletters-api/src/app/routes/newsletterWorkflow.ts
@@ -1,53 +1,65 @@
 import { getPropertyDescription } from '@newsletters-nx/newsletters-data-client';
-import type { WizardLayout } from '@newsletters-nx/state-machine';
+import type {
+	WizardLayout,
+	WizardStepLayout,
+} from '@newsletters-nx/state-machine';
 
-export const newslettersWorkflowStepLayout: WizardLayout = {
-	createNewsletter: {
-		markdownToDisplay: `# Create a new newsletter
+const createNewsletterLayout: WizardStepLayout = {
+	markdownToDisplay: `# Create a new newsletter
 
 This is the first step of the wizard. You can use markdown here.
 On the first step you can ask the user to enter the name of the newsletter.`,
-		buttons: {
-			cancel: {
-				buttonType: 'RED',
-				label: 'Cancel',
-				stepToMoveTo: 'exit',
-			},
-			next: {
-				buttonType: 'GREEN',
-				label: 'Select Template',
-				stepToMoveTo: 'newsletterName',
-				executeStep: async (state, stepLayout): Promise<string | undefined> => {
-					return Promise.resolve('An error occurred');
-				},
+	buttons: {
+		cancel: {
+			buttonType: 'RED',
+			label: 'Cancel',
+			stepToMoveTo: 'exit',
+		},
+		next: {
+			buttonType: 'GREEN',
+			label: 'Select Template',
+			stepToMoveTo: 'newsletterName',
+			executeStep: async (
+				stepData,
+				stepLayout,
+			): Promise<string | undefined> => {
+				return Promise.resolve('An error occurred');
 			},
 		},
 	},
-	exit: {
-		markdownToDisplay: `# Cancelled
+};
 
-This newsletter was cancelled.`,
-		buttons: {},
-	},
-	newsletterName: {
-		markdownToDisplay: `# Newsletter name
+const exitLayout: WizardStepLayout = {
+	markdownToDisplay: `# Cancelled
+
+Creation of the newsletter was cancelled.`,
+	buttons: {},
+};
+
+const newsletterNameLayout: WizardStepLayout = {
+	markdownToDisplay: `# Newsletter name
 
 ${getPropertyDescription('name')}
 
 // Embed an image showing the rendered text.
 ![Newsletter name](https://unsplash.com/photos/5Zg8ZQZJ9qM/download?force=true&w=640)
 `,
-		buttons: {
-			cancel: {
-				buttonType: 'RED',
-				label: 'Cancel',
-				stepToMoveTo: 'exit',
-			},
-			next: {
-				buttonType: 'GREEN',
-				label: 'Select Template',
-				stepToMoveTo: 'selectTemplate',
-			},
+	buttons: {
+		cancel: {
+			buttonType: 'RED',
+			label: 'Cancel',
+			stepToMoveTo: 'exit',
+		},
+		next: {
+			buttonType: 'GREEN',
+			label: 'Select Template',
+			stepToMoveTo: 'selectTemplate',
 		},
 	},
+};
+
+export const newslettersWorkflowStepLayout: WizardLayout = {
+	createNewsletter: createNewsletterLayout,
+	exit: exitLayout,
+	newsletterName: newsletterNameLayout,
 };
