0826d5dcff019c8e1ef8997be3dee96d4b2d9737
Charlotte <43961396+cemms1@users.noreply.github.com>
2023-02-20 14:28:07 +0000

    remove test

--- libs/newsletter-workflows/src/lib/newsletter-workflows.spec.ts
+++
@@ -1,7 +0,0 @@
-import { newsletterWorkflows } from './newsletter-workflows';
-
-describe('newsletterWorkflows', () => {
-	// it('should work', () => {
-	// 	expect(newsletterWorkflows()).toEqual('newsletter-workflows');
-	// });
-});
