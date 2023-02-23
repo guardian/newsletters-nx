c205858b77850a6b063c4675842183c4b1329fbb
Charlotte Emms <43961396+cemms1@users.noreply.github.com>
2023-02-23 17:01:47 +0000

    generate fake newsletters (#32)
    * add faker for generating dummy data
    
    * use deno for fake data generation
    
    * remove generating data fn from main api file
    
    * add console.log to end of deno script
    
    * git commit -m "fix duplicated newsletters and auto format"
    
    * initcap the descriptions
    
    * regenerate local data, remove code and prod data
    
    * uncomment generate script and add printf before formatting part
    
    * use local instead of code data

---
+++ tools/scripts/generate-newsletters.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+red=31
+amber=33
+green=32
+background_color=48
+
+if [[ $(deno --version) ]];
+	then printf "\n\033[1;${green};${background_color}mGenerating fake newsletters...\n\n\033[0m"
+	deno run --allow-write ./tools/scripts/deno/generate-newsletters.js
+	printf "\n\nFormatting newsletters-api project files...\n"
+	npx nx format:write --projects newsletters-api
+	else printf "\n\033[1;${red};${background_color}mNo Deno version found. Please install Deno first.\033[0m\n\033[1;${amber};${background_color}mInstructions here: https://deno.land/manual@v1.30.3/getting_started/installation#download-and-install\033[0m"
+fi;
+
