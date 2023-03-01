2c732c67104e13bbd66f61b8188d1e40b073367b
David Blatcher <dv.bltchr@gmail.com>
2023-03-01 18:39:47 +0000

    remove commented out code

--- libs/state-machine/src/index.ts
+++ libs/state-machine/src/index.ts
@@ -1,6 +1,3 @@
 export * from './lib/stateMachineButtonPressed';
 export * from './lib/getEmptySchemaData';
-
-// exports types from libs/state-machine/src/lib/types.ts
-// Path: libs/state-machine/src/index.ts
 export * from './lib/types';
