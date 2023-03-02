a70abe034238ca4a8f655f4511c1b9170e81c40a
David Blatcher <30567854+dblatcher@users.noreply.github.com>
2023-03-02 13:49:31 +0000

    Dblatcher delete drafts (#41)
    * add delete drafts route and button on the DraftDetails
    
    * move fetching outside the component
    
    * DeleteDraftButton
    
    * can delete from the table view
    
    * add some more test drafts
    
    * correct variable name
    
    * update open-api, use 400 for non-numeric list id
    
    * change api path
    
    * draftStorage uses a failure reason enum
    
    * rename folder for api request functions
    
    * Update apps/newsletters-ui/src/app/lib/requestDraftDeletion.ts
    
    Co-authored-by: Charlotte Emms <43961396+cemms1@users.noreply.github.com>
    
    * remove duplicate key for delete method
    
    ---------
    
    Co-authored-by: Charlotte Emms <43961396+cemms1@users.noreply.github.com>

--- apps/newsletters-api/src/app/routes/drafts.ts
+++ apps/newsletters-api/src/app/routes/drafts.ts
@@ -1,21 +1,41 @@
 import type { FastifyInstance } from 'fastify';
 // import { isLegacyNewsletter } from '@newsletters-nx/newsletters-data-client';
 // import newslettersData from '../../../static/newsletters.local.json';
-import type { DraftStorage } from '@newsletters-nx/newsletters-data-client';
+import type {
+	ApiResponse,
+	DraftStorage,
+	DraftWithId,
+} from '@newsletters-nx/newsletters-data-client';
+import { StorageRequestFailureReason } from '@newsletters-nx/newsletters-data-client';
 import { storageInstance } from '../../services/storageInstance';
 import { makeErrorResponse, makeSuccessResponse } from '../responses';
 
 // casting as DraftStorage so the methods are typed to
 // allow for failed responses.
-const drafStore = storageInstance as DraftStorage;
+const draftStore = storageInstance as DraftStorage;
+
+const mapFailureReasonToStatusCode = (
+	reason?: StorageRequestFailureReason,
+): number => {
+	switch (reason) {
+		case StorageRequestFailureReason.InvalidDataInput:
+			return 400;
+		case StorageRequestFailureReason.NotFound:
+			return 404;
+		default:
+			return 500;
+	}
+};
 
 export function registerDraftsRoutes(app: FastifyInstance) {
 	app.get('/drafts', async (req, res) => {
-		const storageResponse = await drafStore.listDrafts();
+		const storageResponse = await draftStore.listDrafts();
 		if (storageResponse.ok) {
 			return makeSuccessResponse(storageResponse.data);
 		}
-		return makeErrorResponse(storageResponse.message);
+		return res
+			.status(mapFailureReasonToStatusCode(storageResponse.reason))
+			.send(makeErrorResponse(storageResponse.message));
 	});
 
 	app.get<{ Params: { listId: string } }>(
@@ -27,11 +47,39 @@ export function registerDraftsRoutes(app: FastifyInstance) {
 				return makeErrorResponse('Non numerical id passed');
 			}
 
-			const storageResponse = await drafStore.getDraftNewsletter(idAsNumber);
+			const storageResponse = await draftStore.getDraftNewsletter(idAsNumber);
 			if (storageResponse.ok) {
 				return makeSuccessResponse(storageResponse.data);
 			}
-			return makeErrorResponse(storageResponse.message);
+			return res
+				.status(mapFailureReasonToStatusCode(storageResponse.reason))
+				.send(makeErrorResponse(storageResponse.message));
+		},
+	);
+
+	app.delete<{ Params: { listId: string } }>(
+		'/drafts/:listId',
+		async (req, res): Promise<ApiResponse<DraftWithId>> => {
+			const { listId } = req.params;
+			const idAsNumber = Number(listId);
+
+			if (isNaN(idAsNumber)) {
+				return res
+					.status(400)
+					.send(makeErrorResponse('Non numerical id passed'));
+			}
+
+			const storageResponse = await draftStore.deleteDraftNewsletter(
+				idAsNumber,
+			);
+
+			if (storageResponse.ok) {
+				return makeSuccessResponse(storageResponse.data);
+			}
+
+			return res
+				.status(mapFailureReasonToStatusCode(storageResponse.reason))
+				.send(makeErrorResponse(storageResponse.message));
 		},
 	);
 }
