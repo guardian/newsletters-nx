35d9387f62f11262e65b2afa78b3e393efca666c
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2023-03-02 08:13:17 +0000

    Populate calculated fields for draft test data

--- apps/newsletters-api/src/services/storageInstance.ts
+++ apps/newsletters-api/src/services/storageInstance.ts
@@ -1,8 +1,31 @@
 import { InMemoryDraftStorage } from '@newsletters-nx/newsletters-data-client';
 
 const storageInstance = new InMemoryDraftStorage([
-	{ listId: 7000, name: 'Test draft newsletter' },
-	{ listId: 7001, name: 'Other Test draft newsletter', theme: 'news' },
+	{
+		listId: 7000,
+		name: 'Test draft newsletter',
+		identityName: 'test-draft-newsletter',
+		brazeSubscribeEventNamePrefix: 'test_draft_newsletter',
+		brazeNewsletterName: 'Editorial_Testdraftnewsletter',
+		brazeSubscribeAttributeName: 'Testdraftnewsletter_Subscribe_Email',
+		brazeSubscribeAttributeNameAlternate:
+			'email_subscribe_test_draft_newsletter',
+		campaignName: 'Testdraftnewsletter',
+		campaignCode: 'testdraftnewsletter_email',
+	},
+	{
+		listId: 7001,
+		name: 'Other Test draft newsletter',
+		identityName: 'other-test-draft-newsletter',
+		brazeSubscribeEventNamePrefix: 'other_test_draft_newsletter',
+		brazeNewsletterName: 'Editorial_Othertestdraftnewsletter',
+		brazeSubscribeAttributeName: 'Othertestdraftnewsletter_Subscribe_Email',
+		brazeSubscribeAttributeNameAlternate:
+			'email_subscribe_other_test_draft_newsletter',
+		campaignName: 'Othertestdraftnewsletter',
+		campaignCode: 'othertestdraftnewsletter_email',
+		theme: 'news',
+	},
 ]);
 
 export { storageInstance };
