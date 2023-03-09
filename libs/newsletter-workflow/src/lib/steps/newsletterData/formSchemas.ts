6ef92afcc7e3ec61e57feac62a95f14c22931d7a
David Blatcher <dv.bltchr@gmail.com>
2023-03-09 14:31:36 +0000

    add schemas to the steps

---
+++ libs/newsletter-workflow/src/lib/steps/newsletterData/formSchemas.ts
@@ -0,0 +1,48 @@
+import { z } from 'zod';
+import {
+	kebabCasedString,
+	themeEnumSchema,
+	underscoreCasedString,
+} from '@newsletters-nx/newsletters-data-client';
+
+export const formSchemas = {
+	startDraftNewsletter: z
+		.object({
+			name: z.string(),
+		})
+		.describe('Input the name for the new newsletter'),
+
+	identityName: z
+		.object({
+			identityName: kebabCasedString(),
+		})
+		.describe('Edit the identity name if required'),
+
+	braze: z
+		.object({
+			brazeSubscribeEventNamePrefix: underscoreCasedString(),
+			brazeNewsletterName: underscoreCasedString(),
+			brazeSubscribeAttributeName: underscoreCasedString(),
+			brazeSubscribeAttributeNameAlternate: underscoreCasedString(),
+		})
+		.describe('Edit the Braze values if required'),
+
+	ophan: z
+		.object({
+			campaignName: z.string(),
+			campaignCode: z.string(),
+		})
+		.describe('Edit the Ophan values if required'),
+
+	pillar: z
+		.object({
+			theme: themeEnumSchema,
+		})
+		.describe('Choose a theme'),
+
+	description: z
+		.object({
+			description: z.string(),
+		})
+		.describe('Input a short description to display to users'),
+};
