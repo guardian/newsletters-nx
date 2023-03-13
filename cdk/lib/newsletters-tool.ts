ff8df0797abb8bb5dd8038676c3f5aedba96fca2
David Blatcher <dv.bltchr@gmail.com>
2023-03-13 15:55:10 +0000

    set ENV variable in cdk config

--- cdk/lib/newsletters-tool.ts
+++ cdk/lib/newsletters-tool.ts
@@ -45,6 +45,8 @@ export class NewslettersTool extends GuStack {
 			`mkdir -p /opt/${app}`, // make more permanent directory for app to be unzipped into
 			`unzip /tmp/${app}.zip -d /opt/${app}`, // unzip the downloaded zip from /tmp into directory in /opt instead
 			`chown -R ubuntu /opt/${app}`, // change ownership of the copied files to ubuntu user
+			`export NEWSLETTERS_API_READ=true`,
+			`export NEWSLETTERS_UI_SERVE=true`,
 			`su ubuntu -c '/usr/local/node/pm2 start --name ${app} /opt/${app}/dist/apps/newsletters-api/main.cjs'`, // run the main entrypoint file as ubuntu user using pm2
 		].join('\n');
 	};
