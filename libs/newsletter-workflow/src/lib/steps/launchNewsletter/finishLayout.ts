2a8f3bfde096c969869f490f3ef7df179efe1b91
David Blatcher <dv.bltchr@gmail.com>
2023-03-14 10:42:40 +0000

    include the dates in the finish layout

--- libs/newsletter-workflow/src/lib/steps/launchNewsletter/finishLayout.ts
+++ libs/newsletter-workflow/src/lib/steps/launchNewsletter/finishLayout.ts
@@ -1,4 +1,5 @@
 import type { WizardStepLayout } from '@newsletters-nx/state-machine';
+import { getStringValuesFromRecord } from '../../getValuesFromRecord';
 
 const markdownToDisplay = `# Finished
 
@@ -7,5 +8,26 @@ You have reached the end of the wizard. The newsletter has been launched.
 
 export const finishLayout: WizardStepLayout = {
 	staticMarkdown: markdownToDisplay,
+	dynamicMarkdown(requestData, responseData) {
+		const [launchDate, thrasherDate] = getStringValuesFromRecord(
+			responseData ?? {},
+			['launchDate', 'thrasherDate'],
+		);
+
+		const lauchDateString = launchDate
+			? new Date(launchDate).toDateString()
+			: '?';
+		const thrasherDateString = thrasherDate
+			? new Date(thrasherDate).toDateString()
+			: '?';
+
+		return `
+		# Finished
+
+You have reached the end of the wizard.
+ - The newsletter will be launched on ${lauchDateString}.
+ - You have asked for thrashers to be live from ${thrasherDateString}.
+		`.trim();
+	},
 	buttons: {},
 };
