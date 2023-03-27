6aa7cb153483b9b1eeebd3741221686c6774a1ec
David Blatcher <dv.bltchr@gmail.com>
2023-03-27 12:28:36 +0100

    use mui for the ArrayInput

--- apps/newsletters-ui/src/app/components/ArrayInput.tsx
+++ apps/newsletters-ui/src/app/components/ArrayInput.tsx
@@ -1,8 +1,18 @@
+import {
+	Alert,
+	Badge,
+	Box,
+	Button,
+	Grid,
+	TextField,
+	Typography,
+} from '@mui/material';
+import { Fragment } from 'react';
+
 interface Props {
 	data: string[];
 	label: string;
 	change: { (data: string[]): void };
-
 	validationWarning?: string;
 }
 export const ArrayInput = ({
@@ -12,7 +22,7 @@ export const ArrayInput = ({
 	label,
 }: Props) => {
 	const handleInput = (
-		event: React.ChangeEvent<HTMLInputElement>,
+		event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
 		index: number,
 	) => {
 		const dataCopy = [...data];
@@ -32,36 +42,62 @@ export const ArrayInput = ({
 	};
 
 	return (
-		<fieldset>
-			<legend>
-				{label} ({data.length} items)
-			</legend>
-			{validationWarning && <b>!{validationWarning}</b>}
+		<Box paddingY={2} borderTop={1} borderBottom={1}>
+			<Badge badgeContent={data.length} color="primary">
+				<Typography sx={{ fontWeight: 700 }}>{label}</Typography>
+			</Badge>
+			{validationWarning && (
+				<Alert severity="warning">{validationWarning}</Alert>
+			)}
 
-			{data.map((item, index) => (
-				<div key={index}>
-					<input value={item} onChange={(event) => handleInput(event, index)} />
-					<button
-						title={`delete entry "${item}"`}
+			<Grid container alignItems={'center'} rowSpacing={1} columnSpacing={2}>
+				{data.length === 0 && (
+					<Grid item xs={12}>
+						<Alert severity="info">No Items</Alert>
+					</Grid>
+				)}
+
+				{data.map((item, index) => (
+					<Fragment key={index}>
+						<Grid item xs={10}>
+							<TextField
+								variant="standard"
+								fullWidth
+								value={item}
+								onChange={(event) => handleInput(event, index)}
+							/>
+						</Grid>
+						<Grid item xs={2}>
+							<Button
+								size="small"
+								color="error"
+								variant="outlined"
+								title={`delete entry "${item}"`}
+								onClick={() => {
+									handleDelete(index);
+								}}
+							>
+								x
+							</Button>
+						</Grid>
+					</Fragment>
+				))}
+
+				<Grid item xs={10}>
+					<Button
+						fullWidth
+						size="small"
+						variant="outlined"
+						color="success"
+						title={`add new entry to ${label} list`}
 						onClick={() => {
-							handleDelete(index);
+							handleAdd();
 						}}
 					>
-						x
-					</button>
-				</div>
-			))}
-
-			<div>
-				<button
-					title={`add new entry to ${label} list`}
-					onClick={() => {
-						handleAdd();
-					}}
-				>
-					+
-				</button>
-			</div>
-		</fieldset>
+						Add new item
+					</Button>
+				</Grid>
+			</Grid>
+		</Box>
 	);
 };
