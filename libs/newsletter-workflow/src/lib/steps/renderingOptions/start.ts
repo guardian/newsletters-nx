14c795a57b1519b93c26b8da43bd1ef21f9fc31d
David Blatcher <dv.bltchr@gmail.com>
2023-03-30 17:12:26 +0100

    update start copy

--- libs/newsletter-workflow/src/lib/steps/renderingOptions/start.ts
+++ libs/newsletter-workflow/src/lib/steps/renderingOptions/start.ts
@@ -3,7 +3,9 @@ import type { WizardStepLayout } from '@newsletters-nx/state-machine';
 export const startLayout: WizardStepLayout = {
 	staticMarkdown: `# Set Rendering Template Options
 
-This wizard is to choose the options for how your article-based newsletter will appear in Email-rendering.
+This wizard is to choose the options for how an article-based newsletter will appear in Email-rendering.
+
+You do **not need to complete** this wizard for **fronts-based newsletters or newsletters that do not use Email-rendering**.
 
 `,
 	label: 'Start',
