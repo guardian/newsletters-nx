d06fde746c9738015a395f84b87851a2d60d2c2d
David Blatcher <dv.bltchr@gmail.com>
2023-04-03 10:24:29 +0100

    use environment variable to construct s3 storage

--- apps/newsletters-api/src/services/storage/index.ts
+++ apps/newsletters-api/src/services/storage/index.ts
@@ -1,10 +1,14 @@
 import type { DraftStorage } from '@newsletters-nx/newsletters-data-client';
 import { isUsingTestS3Storage } from '../../apiDeploymentSettings';
-import { inMemoryStorageInstance } from './inMemoryStorageInstance';
-import { s3DraftStorage } from './s3StorageInstance';
+import { makeInMemoryStorageInstance } from './inMemoryStorageInstance';
+import { getS3Params } from './s3ParamsFromEnv';
+import { makeS3DraftStorageInstance } from './s3StorageInstance';
 
-const draftStore: DraftStorage = isUsingTestS3Storage()
-	? s3DraftStorage
-	: inMemoryStorageInstance;
+const s3Params = getS3Params();
+const canUseS3 = !!(isUsingTestS3Storage() && s3Params);
+
+const draftStore: DraftStorage = canUseS3
+	? makeS3DraftStorageInstance(s3Params)
+	: makeInMemoryStorageInstance();
 
 export { draftStore };
