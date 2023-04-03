5c44f219af96bc774b48dbe90d0cd4ee20699560
David Blatcher <dv.bltchr@gmail.com>
2023-04-03 11:22:02 +0100

    have a 'use in memory' flag rather than a 'use s3' flag

--- apps/newsletters-api/src/services/storage/index.ts
+++ apps/newsletters-api/src/services/storage/index.ts
@@ -1,12 +1,13 @@
 import type { DraftStorage } from '@newsletters-nx/newsletters-data-client';
-import { isUsingTestS3Storage } from '../../apiDeploymentSettings';
+import { isUsingInMemoryStorage } from '../../apiDeploymentSettings';
 import { makeInMemoryStorageInstance } from './inMemoryStorageInstance';
 import { getS3Params } from './s3ParamsFromEnv';
 import { makeS3DraftStorageInstance } from './s3StorageInstance';
 
 const s3Params = getS3Params();
-const canUseS3 = !!(isUsingTestS3Storage() && s3Params);
+const canUseS3 = !isUsingInMemoryStorage() && s3Params;
 
+// TO DO - how to handle cases in production with missing s3 params?
 const draftStore: DraftStorage = canUseS3
 	? makeS3DraftStorageInstance(s3Params)
 	: makeInMemoryStorageInstance();
