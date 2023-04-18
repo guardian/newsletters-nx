a7d1454a25b4cd0e47e354076661962c5348fd7b
David Blatcher <dv.bltchr@gmail.com>
2023-04-18 08:56:23 +0100

    add simple form page to edit some newsletter properties

--- apps/newsletters-ui/src/app/components/views/NewsletterEditView.tsx
+++ apps/newsletters-ui/src/app/components/views/NewsletterEditView.tsx
@@ -1,6 +1,6 @@
 import { Link, useLoaderData } from 'react-router-dom';
-import type { NewsletterData } from '@newsletters-nx/newsletters-data-client';
 import { isNewsletterData } from '@newsletters-nx/newsletters-data-client';
+import { NewsletterEditForm } from '../EditNewsletterForm';
 
 export const NewsletterEditView = () => {
 	const matchedItem = useLoaderData();
@@ -12,36 +12,9 @@ export const NewsletterEditView = () => {
 		return <article>NOT VALID DATA!</article>;
 	}
 
-	const testPatch = () => {
-		console.log('trying to patch the description');
-		const modification: Partial<NewsletterData> = {
-			description: `Description updated at ${new Date().toTimeString()}`,
-		};
-
-		fetch(`/api/newsletters/${matchedItem.listId}`, {
-			method: 'PATCH',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(modification),
-		})
-			.then((response) => {
-				console.log(response);
-			})
-			.catch((error) => {
-				console.warn('failed!');
-				console.log(error);
-			});
-	};
-
 	return (
 		<>
-			<h2>EDIT:</h2>
-			<p>{matchedItem.identityName}</p>
-			<p>{matchedItem.description}</p>
-
-			<button onClick={testPatch}>change description</button>
-
+			<NewsletterEditForm originalItem={matchedItem} />
 			<Link to="/newsletters/">Back to List</Link>
 		</>
 	);
