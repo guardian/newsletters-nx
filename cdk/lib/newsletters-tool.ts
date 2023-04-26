c2c5dcf0457798ae197ad838e2e92a0e6d292394
Phill Barron <phillipbarron242@gmail.com>
2023-04-26 13:43:00 +0100

    removed acl access in s3 policy

--- cdk/lib/newsletters-tool.ts
+++ cdk/lib/newsletters-tool.ts
@@ -86,9 +86,7 @@ export class NewslettersTool extends GuStack {
 					effect: Effect.ALLOW,
 					actions: [
 						's3:PutObject',
-						's3:PutObjectAcl',
 						's3:GetObject',
-						's3:GetObjectAcl',
 						's3:GetObjectVersion',
 						's3:DeleteObject',
 						's3:ListBucket',
