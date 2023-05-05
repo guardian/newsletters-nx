e7e151aa973546b617bb34eab365aca937ede9ec
David Blatcher <dv.bltchr@gmail.com>
2023-05-05 10:35:32 +0100

    avoid checking the schemas on every render

--- apps/newsletters-ui/src/app/components/StepNav.tsx
+++ apps/newsletters-ui/src/app/components/StepNav.tsx
@@ -1,4 +1,5 @@
 import { css, Step, StepButton, StepLabel, Stepper } from '@mui/material';
+import { useState } from 'react';
 import type {
 	StepListing,
 	StepperConfig,
@@ -20,6 +21,17 @@ export const StepNav = ({
 	handleStepClick,
 	formData,
 }: Props) => {
+	// Validating formData aginst the schema for every step to see if the
+	// step is complete is potentially a fairly expensive operation.
+	// The state logic is so this is done only when the step changes,
+	// not every time the user changes the formData (which includes every
+	// key pressed in a text field).
+	const [currentStepIdOnLastRender, setCurrenStepIdOnLastRender] =
+		useState(currentStepId);
+	const [completionRecord, setCompletionRecord] = useState<
+		Partial<Record<string, boolean>>
+	>({});
+
 	const filteredStepList = stepperConfig.steps.filter((step) => {
 		if (step.parentStepId) {
 			return false;
@@ -37,6 +49,32 @@ export const StepNav = ({
 		}
 	});
 
+	const updateCompletion = () => {
+		const list = stepperConfig.steps.reduce<Partial<Record<string, boolean>>>(
+			(record, step) => {
+				const result = step.schema
+					? step.schema.safeParse(formData).success
+					: true;
+
+				return { ...record, [step.id]: result };
+			},
+			{},
+		);
+
+		setCompletionRecord(list);
+	};
+
+	// On the initial render, the completionRecord is set to {}
+	if (!Object.keys(completionRecord).length) {
+		updateCompletion();
+	}
+
+	// Update the completion record if the step has changed
+	if (currentStepId !== currentStepIdOnLastRender) {
+		setCurrenStepIdOnLastRender(currentStepId);
+		updateCompletion();
+	}
+
 	const currentStep = stepperConfig.steps.find(
 		(step) => step.id === currentStepId,
 	);
@@ -44,14 +82,13 @@ export const StepNav = ({
 	const isCurrent = (step: StepListing) =>
 		step.id === currentStep?.id || step.id === currentStep?.parentStepId;
 
+	const isComplete = (step: StepListing) => completionRecord[step.id];
+
 	const shouldRenderAsButton = (step: StepListing) =>
 		currentStep?.canSkipFrom &&
 		stepperConfig.isNonLinear &&
 		step.canSkipTo &&
 		!isCurrent(step);
-
-	const isComplete = (step: StepListing) =>
-		step.schema ? step.schema.safeParse(formData).success : true;
 
 	return (
 		<Stepper
