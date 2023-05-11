e414d2f44578a2e6d0d06eb36fb74bbe4e2a3e3e
David Blatcher <dv.bltchr@gmail.com>
2023-05-11 14:25:14 +0100

    add info message

--- apps/newsletters-ui/src/app/components/NewsletterJsonEdit.tsx
+++ apps/newsletters-ui/src/app/components/NewsletterJsonEdit.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { Alert, Typography } from '@mui/material';
 import type { NewsletterData } from '@newsletters-nx/newsletters-data-client';
 import { newsletterDataSchema } from '@newsletters-nx/newsletters-data-client';
 import { requestNewsletterEdit } from '../api-requests/request-newsletter-edit';
@@ -29,7 +29,24 @@ export const NewsletterJsonEdit = ({ newsletter }: Props) => {
 
 	return (
 		<>
-			<Typography variant="h2">edit json</Typography>
+			<Typography variant="h2">
+				edit json for {newsletter.identityName}
+			</Typography>
+			<Alert severity="info">
+				<Typography>
+					This tool is intended for developer use only. Note that:
+				</Typography>
+				<ul>
+					<Typography component={'li'}>
+						This tool is for editting an existing newsletter - it can't be used
+						to create a new one or to copy/duplicate the newsletter
+					</Typography>
+					<Typography component={'li'}>
+						the "listId" and "identityName" fields in the JSON below will be
+						ignored as the API will not allow these values to be editted.
+					</Typography>
+				</ul>
+			</Alert>
 			<JsonEdittor
 				originalData={newsletter}
 				schema={newsletterDataSchema}
