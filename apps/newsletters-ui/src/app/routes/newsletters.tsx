bc1935d045d61a31ad91ce8c4fe11218713e0635
David Blatcher <30567854+dblatcher@users.noreply.github.com>
2023-05-16 11:37:48 +0100

    Dblatcher remove demo code (#135)
    * delete the button, route and page for the legacy create form
    
    * delete the legacy newsletter form and its imports
    
    * delete legacy property tabel
    
    * remove the demo forms
    
    * make the UpdateNewsletter button work

--- apps/newsletters-ui/src/app/routes/newsletters.tsx
+++ apps/newsletters-ui/src/app/routes/newsletters.tsx
@@ -1,6 +1,4 @@
 import type { RouteObject } from 'react-router-dom';
-import { FormDemoView } from '../components/FormDemoView';
-import { NewsletterCreateView } from '../components/views/NewsletterCreateView';
 import { NewsletterDetailView } from '../components/views/NewsletterDetailView';
 import { NewsletterEditView } from '../components/views/NewsletterEditView';
 import { NewslettersListView } from '../components/views/NewslettersListView';
@@ -28,15 +26,6 @@ export const newslettersRoute: RouteObject = {
 			path: 'edit/:id',
 			element: <NewsletterEditView />,
 			loader: detailLoader,
-		},
-		{
-			path: 'create',
-			element: <NewsletterCreateView />,
-			loader: listLoader,
-		},
-		{
-			path: 'forms',
-			element: <FormDemoView />,
 		},
 		{
 			path: 'newsletter-data/:listId',
