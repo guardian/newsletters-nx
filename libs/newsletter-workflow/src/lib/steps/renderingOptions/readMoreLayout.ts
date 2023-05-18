8903fea6beda48f396873c0473afe7b88b80fcd7
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2023-05-18 11:03:03 +0100

    Add combined validation for Read More configurations

--- libs/newsletter-workflow/src/lib/steps/renderingOptions/readMoreLayout.ts
+++ libs/newsletter-workflow/src/lib/steps/renderingOptions/readMoreLayout.ts
@@ -45,10 +45,35 @@ export const readMoreLayout: WizardStepLayout<DraftStorage> = {
 			stepToMoveTo: getPreviousOrEditStartStepId,
 			executeStep: executeModify,
 		},
-		finish: {
+		next: {
 			buttonType: 'NEXT',
 			label: 'Next',
 			stepToMoveTo: getNextStepId,
+			onBeforeStepChangeValidate: (stepData): string | undefined => {
+				const readMoreDetails = stepData.formData
+					? (stepData.formData[
+							'renderingOptions.readMoreSections'
+					  ] as unknown as Array<{
+							subheading: string;
+							wording: string;
+							url: string;
+					  }>)
+					: undefined;
+				if (readMoreDetails) {
+					if (Array.isArray(readMoreDetails)) {
+						const invalidReadMoreDetails = readMoreDetails.filter(
+							(readMoreEntry) =>
+								!readMoreEntry.subheading ||
+								!readMoreEntry.wording ||
+								!readMoreEntry.url,
+						);
+						if (invalidReadMoreDetails.length > 0) {
+							return 'ALL READ MORE DETAILS MUST BE SPECIFIED FOR A CONFIGURATION';
+						}
+					}
+				}
+				return undefined;
+			},
 			executeStep: executeModify,
 		},
 	},
