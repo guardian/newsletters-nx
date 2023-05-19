14aef7b15978235a131fffb822f97fcca7d5d17e
David Blatcher <30567854+dblatcher@users.noreply.github.com>
2023-05-19 16:39:45 +0100

    add floating action button to navigate to new draft wizard (#143)
    * add floating action button to navigate to new draft wizard
    
    * use primary color
    
    * aria label

--- apps/newsletters-ui/src/app/components/views/DraftListView.tsx
+++ apps/newsletters-ui/src/app/components/views/DraftListView.tsx
@@ -1,6 +1,9 @@
+import AddIcon from '@mui/icons-material/Add';
+import { Box, Container } from '@mui/material';
 import { useLoaderData } from 'react-router-dom';
 import { isDraft } from '@newsletters-nx/newsletters-data-client';
 import { DraftsTable } from '../DraftsTable';
+import { NavigateFab } from '../NavigateFab';
 
 export const DraftListView = () => {
 	const list = useLoaderData();
@@ -9,5 +12,22 @@ export const DraftListView = () => {
 	}
 
 	const drafts = list.filter(isDraft);
-	return <DraftsTable drafts={drafts} />;
+	return (
+		<>
+			<DraftsTable drafts={drafts} />
+			<Container maxWidth="lg">
+				<Box paddingX={1} display={'flex'} justifyContent={'flex-end'}>
+					<NavigateFab
+						href="/newsletters/newsletter-data"
+						color="primary"
+						variant="extended"
+						aria-label="open create new draft wizard"
+					>
+						create new draft
+						<AddIcon />
+					</NavigateFab>
+				</Box>
+			</Container>
+		</>
+	);
 };
