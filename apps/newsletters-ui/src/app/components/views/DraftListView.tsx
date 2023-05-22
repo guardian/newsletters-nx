4fa16e81efde9983f7a152de921c386a3dc2161f
Phill Barron <phillipbarron242@gmail.com>
2023-05-22 10:53:36 +0100

    Using the built-in Navigate functionality

--- apps/newsletters-ui/src/app/components/views/DraftListView.tsx
+++ apps/newsletters-ui/src/app/components/views/DraftListView.tsx
@@ -1,12 +1,11 @@
 import AddIcon from '@mui/icons-material/Add';
 import { Box, Button, Container } from '@mui/material';
-import { useLoaderData, useNavigate } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 import { isDraft } from '@newsletters-nx/newsletters-data-client';
 import { DraftsTable } from '../DraftsTable';
 
 export const DraftListView = () => {
 	const list = useLoaderData();
-	const navigate = useNavigate();
 	if (!list || !Array.isArray(list)) {
 		return <nav>No Drafts</nav>;
 	}
@@ -25,7 +24,7 @@ export const DraftListView = () => {
 					<Button
 						variant="contained"
 						endIcon={<AddIcon />}
-						onClick={() => navigate('/newsletters/newsletter-data')}
+						href={'/newsletters/newsletter-data'}
 						aria-label="open create new draft wizard"
 					>
 						New draft
