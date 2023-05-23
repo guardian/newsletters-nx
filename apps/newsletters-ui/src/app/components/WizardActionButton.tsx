16492c89d2edacafb2e17eea49e9c1f303431696
David Blatcher <30567854+dblatcher@users.noreply.github.com>
2023-05-23 08:13:06 +0100

    Dblatcher wizard style tweaks (#148)
    * factor out the wizard buttons to a component
    
    * remove deps
    
    * replce manual styled div
    
    * use unitless values for sx
    
    * simplify styling for left-aligned-step-button
    
    * refactor fetch step to use async-await
    
    * rename

---
+++ apps/newsletters-ui/src/app/components/WizardActionButton.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@mui/material';
+import type { WizardButtonType } from '@newsletters-nx/newsletters-data-client';
+import type { WizardButton } from '@newsletters-nx/state-machine';
+
+interface Props {
+	button: WizardButton;
+	onClick: { (buttonId: string): { (): void } };
+}
+
+export const WizardActionButton = ({ button, onClick }: Props) => {
+	const primaryActions: WizardButtonType[] = ['NEXT', 'LAUNCH'];
+
+	const variant = primaryActions.includes(button.buttonType)
+		? 'contained'
+		: 'outlined';
+
+	return (
+		<Button
+			variant={variant}
+			onClick={() => {
+				onClick(button.id)();
+			}}
+		>
+			{button.label}
+		</Button>
+	);
+};
