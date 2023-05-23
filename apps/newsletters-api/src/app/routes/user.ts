1a9ec12df22847a2590a4b771a1b66fd723b0aa9
David Blatcher <dv.bltchr@gmail.com>
2023-05-23 15:35:58 +0100

    decode jwt data from header

--- apps/newsletters-api/src/app/routes/user.ts
+++ apps/newsletters-api/src/app/routes/user.ts
@@ -1,9 +1,38 @@
 import type { FastifyInstance } from 'fastify';
 
-const fakeCookie = process.env.FAKE_COOKIE;
+const { FAKE_JWT } = process.env;
+
+const USE_FAKE_JWT = true as boolean;
+
+const atob = (a: string) => Buffer.from(a, 'base64').toString('binary');
+
+function parseJwt(token: string): Partial<Record<string, string>> {
+	try {
+		const base64Url = token.split('.')[1] as string;
+		const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+		const jsonPayload = decodeURIComponent(
+			atob(base64)
+				.split('')
+				.map(function (c) {
+					return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+				})
+				.join(''),
+		);
+		return JSON.parse(jsonPayload) as Partial<Record<string, string>>;
+	} catch (err: unknown) {
+		console.warn('failed to parseJwt', err);
+		return {};
+	}
+}
 
 export function registerUserRoute(app: FastifyInstance) {
-	app.get('/api/user/whoami', (req, res) => {
-		return res.send({ message: 'IDK', fakeCookie });
+	app.get('/api/user/whoami', async (req, res) => {
+		const jwtProfile = USE_FAKE_JWT
+			? FAKE_JWT
+			: req.headers['x-amzn-oidc-data'];
+
+		const profile = typeof jwtProfile === 'string' ? parseJwt(jwtProfile) : {};
+
+		return res.send(profile);
 	});
 }
