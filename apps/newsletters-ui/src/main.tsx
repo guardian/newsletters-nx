95233649736a1286874994796a5afb77013b5aff
David Blatcher <dv.bltchr@gmail.com>
2023-05-25 09:23:51 +0100

    move import again??

--- apps/newsletters-ui/src/main.tsx
+++ apps/newsletters-ui/src/main.tsx
@@ -2,11 +2,11 @@ import { ThemeProvider } from '@mui/material';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { appTheme } from './app-theme';
 import { DefaultStyles } from './app/components/DefaultStyles';
 import { draftRoute } from './app/routes/drafts';
 import { homeRoute } from './app/routes/home';
 import { newslettersRoute } from './app/routes/newsletters';
-import { appTheme } from './app-theme';
 
 const router = createBrowserRouter([homeRoute, newslettersRoute, draftRoute]);
 
