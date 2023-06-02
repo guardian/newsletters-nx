fa6e65b64e5ee3eb59a79e3ef71a68bf4bd50718
David Blatcher <dv.bltchr@gmail.com>
2023-06-02 13:51:43 +0100

    use hooks for fetching user info in the UI

--- apps/newsletters-api/src/app/routes/user.ts
+++ apps/newsletters-api/src/app/routes/user.ts
@@ -1,15 +1,20 @@
 import type { FastifyInstance } from 'fastify';
+import { getPermissions } from '@newsletters-nx/newsletters-data-client';
 import { getUserProfile } from '../get-user-profile';
 import { makeErrorResponse, makeSuccessResponse } from '../responses';
 
 export function registerUserRoute(app: FastifyInstance) {
 	app.get('/api/user/whoami', async (req, res) => {
 		const maybeUser = getUserProfile(req);
-
 		if (!maybeUser.profile) {
 			return res.status(500).send(makeErrorResponse(maybeUser.errorMessage));
 		}
-
 		return res.send(makeSuccessResponse(maybeUser.profile));
+	});
+
+	app.get('/api/user/permissions', async (req, res) => {
+		const maybeUser = getUserProfile(req);
+		const permissions = await getPermissions(maybeUser.profile);
+		return res.send(makeSuccessResponse(permissions));
 	});
 }
