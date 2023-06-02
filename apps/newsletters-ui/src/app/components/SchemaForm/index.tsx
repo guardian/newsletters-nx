9f8f2843e7e4f65a4165c44f3b99f6da05db06bc
David Blatcher <30567854+dblatcher@users.noreply.github.com>
2023-06-02 08:52:29 +0100

    Dblatcher radio enum fields (#165)
    * use radio buttons instead of a drop down when there are five or fewer options
    
    * pass the max options prop to all uses of the schema form

--- apps/newsletters-ui/src/app/components/SchemaForm/index.tsx
+++ apps/newsletters-ui/src/app/components/SchemaForm/index.tsx
@@ -17,6 +17,7 @@ interface Props<T extends z.ZodRawShape> {
 	excludedKeys?: string[];
 	readOnlyKeys?: string[];
 	validationWarnings: Partial<Record<keyof T, string>>;
+	maxOptionsForRadioButtons?: number;
 }
 
 const getArrayItemTypeAndRecordSchema = (
@@ -50,6 +51,7 @@ export function SchemaForm<T extends z.ZodRawShape>({
 	excludedKeys = [],
 	readOnlyKeys = [],
 	validationWarnings,
+	maxOptionsForRadioButtons = 0,
 }: Props<T>) {
 	const fields: FieldDef[] = [];
 	for (const key in schema.shape) {
@@ -97,6 +99,7 @@ export function SchemaForm<T extends z.ZodRawShape>({
 					showUnsupported={showUnsupported}
 					stringInputType={field.key === 'text' ? 'textArea' : undefined}
 					validationWarning={validationWarnings[field.key]}
+					maxOptionsForRadioButtons={maxOptionsForRadioButtons}
 				/>
 			))}
 		</article>
