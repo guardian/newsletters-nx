4daf27093e4c919d3fad5578ebc8cd2799c903bb
Phill Barron <phillipbarron242@gmail.com>
2023-06-07 21:49:38 +0100

    Updated to describe a developer profile as this is required for launch .

--- apps/newsletters-api/src/services/permissions/LocalPermissions.ts
+++ apps/newsletters-api/src/services/permissions/LocalPermissions.ts
@@ -1,20 +1,11 @@
-import type { UserProfile } from '@newsletters-nx/newsletters-data-client';
-import {
-	levelToPermissions,
-	UserAccessLevel,
-} from '@newsletters-nx/newsletters-data-client';
-import { getLocalUserProfiles } from '../../apiDeploymentSettings';
-import type { PermissionsService } from './abstract-class';
+import type {UserProfile} from '@newsletters-nx/newsletters-data-client';
+import {levelToPermissions, UserAccessLevel,} from '@newsletters-nx/newsletters-data-client';
+import {getLocalUserProfiles} from '../../apiDeploymentSettings';
+import type {PermissionsService} from './abstract-class';
 
 const localUsers = getLocalUserProfiles();
 
-const getDefaultUserAccessLevel = (): UserAccessLevel => {
-	const { DEFAULT_USER_ACCESS_LEVEL } = process.env;
-	if (DEFAULT_USER_ACCESS_LEVEL) {
-		return parseInt(DEFAULT_USER_ACCESS_LEVEL, 10) as UserAccessLevel;
-	}
-	return UserAccessLevel.Viewer;
-};
+
 /**
  * Placeholder function using static user data. Async to
  * simulate a call to the Google Groups API (or similar)
@@ -27,7 +18,7 @@ const getUserAccessLevel = async (
 
 	return user?.email
 		? userList[user.email] ?? UserAccessLevel.Viewer
-		: getDefaultUserAccessLevel();
+		: UserAccessLevel.Viewer
 };
 
 export class LocalPermissionService implements PermissionsService {
