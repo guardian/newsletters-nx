a1d4974c11031d2b783acfc697278785bee4040e
David Blatcher <dv.bltchr@gmail.com>
2023-07-05 15:46:13 +0100

    add test for new function

--- libs/newsletters-data-client/src/lib/json-undefined-null-conversions.spec.ts
+++ libs/newsletters-data-client/src/lib/json-undefined-null-conversions.spec.ts
@@ -1,4 +1,7 @@
-import { replaceNullWithUndefined } from './json-undefined-null-conversions';
+import {
+	replaceNullWithUndefinedForUnknown,
+	replaceNullWithUndefinedInFormDataRecord,
+} from './json-undefined-null-conversions';
 import type { FormDataRecord } from './transformWizardData';
 
 const SIMPLE_IN: FormDataRecord = {
@@ -55,17 +58,73 @@ const OBJECT_ARRAY_OUT: FormDataRecord = {
 	],
 };
 
-describe('replaceNullWithUndefined', () => {
+describe('replaceNullWithUndefinedInFormDataRecord', () => {
 	it('will handle simple cases', () => {
-		const result = replaceNullWithUndefined({ ...SIMPLE_IN });
+		const result = replaceNullWithUndefinedInFormDataRecord({ ...SIMPLE_IN });
 		expect(result).toEqual(SIMPLE_OUT);
 	});
 	it('will handle nulls in nested objects', () => {
-		const result = replaceNullWithUndefined({ ...OBJECT_IN });
+		const result = replaceNullWithUndefinedInFormDataRecord({ ...OBJECT_IN });
 		expect(result).toEqual(OBJECT_OUT);
 	});
 	it('will handle nulls in arrays of object', () => {
-		const result = replaceNullWithUndefined({ ...OBJECT_ARRAY_IN });
+		const result = replaceNullWithUndefinedInFormDataRecord({
+			...OBJECT_ARRAY_IN,
+		});
 		expect(result).toEqual(OBJECT_ARRAY_OUT);
+	});
+});
+
+describe('replaceNullWithUndefinedForUnknown', () => {
+	it('will handle simple cases', () => {
+		const result = replaceNullWithUndefinedForUnknown({ ...SIMPLE_IN });
+		expect(result).toEqual(SIMPLE_OUT);
+	});
+	it('will handle nulls in nested objects', () => {
+		const result = replaceNullWithUndefinedForUnknown({ ...OBJECT_IN });
+		expect(result).toEqual(OBJECT_OUT);
+	});
+	it('will handle nulls in arrays of object', () => {
+		const result = replaceNullWithUndefinedForUnknown({
+			...OBJECT_ARRAY_IN,
+		});
+		expect(result).toEqual(OBJECT_ARRAY_OUT);
+	});
+
+	it('will convert null primitives to undefined', () => {
+		const result = replaceNullWithUndefinedForUnknown(null);
+		expect(result).toEqual(undefined);
+	});
+	it('will convert null primitives to in an array to undefined', () => {
+		const result = replaceNullWithUndefinedForUnknown([
+			1,
+			null,
+			'this is a string',
+		]);
+		expect(result).toEqual([1, undefined, 'this is a string']);
+	});
+	it('will convert null deeply nested nulls to in an object to undefined', () => {
+		const result = replaceNullWithUndefinedForUnknown({
+			a: {
+				value: null,
+				b: {
+					value: 'test',
+					c: {
+						value: null,
+					},
+				},
+			},
+		});
+		expect(result).toEqual({
+			a: {
+				value: undefined,
+				b: {
+					value: 'test',
+					c: {
+						value: undefined,
+					},
+				},
+			},
+		});
 	});
 });
