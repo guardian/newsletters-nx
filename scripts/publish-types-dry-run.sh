55f04900009bef4d9a95a21ebff0939ff8f3a801
David Blatcher <dv.bltchr@gmail.com>
2023-07-11 16:47:57 +0100

    bump version and build project in release script

--- scripts/publish-types-dry-run.sh
+++ scripts/publish-types-dry-run.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+# MOVING BACK TO SOURCE FOLDER
+cd ./libs/editorial-newsletters-ts
+echo ""
+echo "**"
+echo "Changing editorial-newsletters-ts version:"
+npm version minor
+echo "**"
+echo ""
+
+# MOVING BACK TO ROOT
+cd ../../
+nx run editorial-newsletters-ts:build
+
+# MOVING TO DIST FOLDER
+cd ./dist/libs/editorial-newsletters-ts
 echo "publishing dry run... "
-cd /dist/libs/editorial-newsletters-ts
 npm publish --dry-run
+
+# MOVING BACK TO ROOT
+cd ../../
