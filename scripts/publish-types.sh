1f0acf7c0ae5d9652ef3acad61cc33ca5991d959
David Blatcher <dv.bltchr@gmail.com>
2023-07-12 09:07:04 +0100

    check for changesets before running version

--- scripts/publish-types.sh
+++ scripts/publish-types.sh
@@ -2,7 +2,20 @@
 
 # MOVING BACK TO SOURCE FOLDER
 cd ./libs/editorial-newsletters-ts
+
+# check there are valid changesets to add to the next version
+npx changeset status
+status=$?
+if [ $status -gt 0 ]
+	then
+	exit 1
+fi
+
+
+# https://github.com/changesets/changesets/blob/main/packages/cli/README.md#version :
 npx changeset version
+# From changeset docs:
+# "We recommend making sure changes made from this command are merged back into the base branch before you run publish."
 
 # MOVING BACK TO ROOT
 cd ../../
@@ -10,7 +23,7 @@ nx run editorial-newsletters-ts:build
 
 # MOVING TO DIST FOLDER
 cd ./dist/libs/editorial-newsletters-ts
-echo "publishing dry run... "
+echo "publishing package"
 npx changeset publish
 
 # MOVING BACK TO ROOT
