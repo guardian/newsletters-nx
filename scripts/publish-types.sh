05fe3399aed477962b06bcb2a6c04f60dbe47394
David Blatcher <dv.bltchr@gmail.com>
2023-07-12 12:41:19 +0100

    correct publish script - no cd command

--- scripts/publish-types.sh
+++ scripts/publish-types.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-# MOVING TO DIST FOLDER
-cd ./dist/libs/editorial-newsletters-ts
 echo "publishing package"
 npx changeset publish
-# https://github.com/guardian/recommendations/blob/main/npm-packages.md#publishing
-# For this to work, need the NPM_TOKEN secret
 
 
