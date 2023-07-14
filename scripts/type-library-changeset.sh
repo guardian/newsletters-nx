e8f5b93e4c3e99626aed7bf2aa1828f999b66528
Phill Barron <phillipbarron242@gmail.com>
2023-07-14 15:59:40 +0100

    removing changesets and moving psh to separate workflow

--- scripts/type-library-changeset.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# MOVING BACK TO SOURCE FOLDER
-cd ./libs/editorial-newsletters-ts
-
-npx changeset
--- scripts/type-library-publish.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# MOVING BACK TO SOURCE FOLDER
-cd ./libs/editorial-newsletters-ts || exit
-
-npx changeset publish --access=public
