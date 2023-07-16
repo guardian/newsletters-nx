d08b039a8c2a85608f818bff1a8ff254b5361230
Phill Barron <phillipbarron242@gmail.com>
2023-07-16 08:16:32 +0100

    tweak script

--- scripts/publish-types.sh
+++ scripts/publish-types.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Run npm publish command
 npm publish --access public
 
-# Check the exit code of the previous command
 exit_code=$?
 
-# If the exit code is 1 (error), check if it's a 403 error
 if [[ $exit_code -eq 1 ]]; then
     if [[ $(npm publish 2>&1) == *"403"* ]]; then
         echo "Error: Publishing failed due to a 403 Forbidden error. Package may not have required an update"
     else
         echo "Error: Publishing failed."
+        exit 1
     fi
 else
     echo "Success: Package published successfully."
