f6a337ea7bd2d3fed56b4c9c4803581e32433c13
David Blatcher <dv.bltchr@gmail.com>
2023-07-17 16:50:30 +0100

    fake delay, loading indicator, useCallback for fetch function

--- apps/newsletters-ui/src/app/components/TemplatePreview.tsx
+++ apps/newsletters-ui/src/app/components/TemplatePreview.tsx
@@ -1,5 +1,6 @@
+import RefreshIcon from '@mui/icons-material/Refresh';
 import { Box, Button, ButtonGroup, Container } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import type { NewsletterData } from '@newsletters-nx/newsletters-data-client';
 import { fetchApiData, fetchPostApiData } from '../api-requests/fetch-api-data';
 
@@ -9,42 +10,56 @@ interface Props {
 	minHeight?: number;
 }
 
+const sleep = (time: number) =>
+	new Promise((resolve) => {
+		setTimeout(resolve, time);
+	});
+
 export const TemplatePreview = ({
 	identityName,
 	newsletterData,
 	minHeight = 800,
 }: Props) => {
 	const [content, setContent] = useState<string | undefined>(undefined);
+	const [waitingForUpdate, setWaitingForUpdate] = useState(false);
 	const [frameWidth, setFrameWidth] = useState(360);
 
+	const fetchData = useCallback(async () => {
+		if (!identityName && !newsletterData) {
+			setWaitingForUpdate(false);
+			setContent(undefined);
+			return;
+		}
+
+		const delay = 100 + 100 * Math.floor(Math.random() * 10);
+		await sleep(delay);
+
+		if (identityName) {
+			const data = await fetchApiData<{ content: string }>(
+				`/api/rendering-templates/preview/${identityName}`,
+			);
+			setWaitingForUpdate(false);
+			if (data) {
+				setContent(data.content);
+			}
+		}
+
+		if (newsletterData) {
+			const data = await fetchPostApiData<{ content: string }>(
+				`/api/rendering-templates/preview`,
+				newsletterData,
+			);
+			setWaitingForUpdate(false);
+			if (data) {
+				setContent(data.content);
+			}
+		}
+	}, [identityName, newsletterData]);
+
 	useEffect(() => {
-		const fetchData = async () => {
-			if (!identityName && !newsletterData) {
-				return setContent(undefined);
-			}
-
-			if (identityName) {
-				const data = await fetchApiData<{ content: string }>(
-					`/api/rendering-templates/preview/${identityName}`,
-				);
-				if (data) {
-					setContent(data.content);
-				}
-			}
-
-			if (newsletterData) {
-				const data = await fetchPostApiData<{ content: string }>(
-					`/api/rendering-templates/preview`,
-					newsletterData,
-				);
-				if (data) {
-					setContent(data.content);
-				}
-			}
-		};
-
+		setWaitingForUpdate(true);
 		void fetchData();
-	}, [content, identityName, newsletterData]);
+	}, [fetchData]);
 
 	const WidthButton = (props: { width: number }) => (
 		<Button
@@ -59,7 +74,12 @@ export const TemplatePreview = ({
 
 	return (
 		<Container
-			sx={{ paddingTop: 1, borderStyle: 'solid', borderColor: 'primary.dark' }}
+			sx={{
+				paddingTop: 1,
+				borderStyle: 'solid',
+				borderColor: 'primary.dark',
+				position: 'relative',
+			}}
 		>
 			<ButtonGroup>
 				<WidthButton width={320} />
@@ -67,6 +87,12 @@ export const TemplatePreview = ({
 				<WidthButton width={425} />
 				<WidthButton width={650} />
 			</ButtonGroup>
+
+			{waitingForUpdate && (
+				<Box sx={{ position: 'absolute', top: 0, right: 0 }}>
+					<RefreshIcon sx={{ fontSize: 160 }} color="secondary" />
+				</Box>
+			)}
 
 			<Box
 				width={'100%'}
