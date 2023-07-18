79bf8fff59bf827cff328201c8544bb4e25be5b4
David Blatcher <dv.bltchr@gmail.com>
2023-07-18 11:51:09 +0100

    don't need the loader on the preview page

--- apps/newsletters-ui/src/app/components/RenderingPreviewPage.tsx
+++ apps/newsletters-ui/src/app/components/RenderingPreviewPage.tsx
@@ -1,6 +1,9 @@
 import { ButtonGroup, Typography } from '@mui/material';
+import { useCallback, useEffect, useState } from 'react';
 import type { NewsletterData } from '@newsletters-nx/newsletters-data-client';
+import { fetchApiData } from '../api-requests/fetch-api-data';
 import { NavigateButton } from './NavigateButton';
+import { TemplatePreview } from './TemplatePreview';
 import { TemplatePreviewLoader } from './TemplatePreviewLoader';
 
 interface Props {
@@ -8,12 +11,28 @@ interface Props {
 }
 
 export const RenderingPreviewPage = ({ newsletter }: Props) => {
+	const [content, setContent] = useState<string | undefined>(undefined);
+
+	const fetchData = useCallback(async () => {
+		const data = await fetchApiData<{ content: string }>(
+			`/api/rendering-templates/preview/${newsletter.identityName}`,
+		);
+		if (data) {
+			setContent(data.content);
+		}
+	}, [newsletter]);
+
+	useEffect(() => {
+		console.log('fetch');
+		void fetchData();
+	}, [fetchData]);
+
 	return (
 		<>
 			<Typography variant="h2">
 				Email rendering preview: {newsletter.name}
 			</Typography>
-			<TemplatePreviewLoader identityName={newsletter.identityName} />
+			<TemplatePreview html={content} isLoading={!content} />
 
 			<ButtonGroup>
 				<NavigateButton href={`/newsletters/${newsletter.identityName}`}>
