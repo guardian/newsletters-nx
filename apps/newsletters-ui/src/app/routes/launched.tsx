f8386a99a3aa3d67595977b91c4f3b25c492c9b1
Phill Barron <phillipbarron242@gmail.com>
2023-07-24 12:05:50 +0100

    correct route

--- apps/newsletters-ui/src/app/routes/launched.tsx
+++ apps/newsletters-ui/src/app/routes/launched.tsx
@@ -3,7 +3,7 @@ import { NewsletterDetailView } from '../components/views/NewsletterDetailView';
 import { NewsletterEditView } from '../components/views/NewsletterEditView';
 import { NewsletterJsonEditView } from '../components/views/NewsletterJsonEditView';
 import { NewslettersListView } from '../components/views/NewslettersListView';
-import { PreviewView } from "../components/views/PreviewView";
+import { PreviewView } from '../components/views/PreviewView';
 import { RenderingOptionsView } from '../components/views/RenderingOptionsView';
 import { ErrorPage } from '../ErrorPage';
 import { Layout } from '../Layout';
