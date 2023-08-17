a886907f41b19e5b85e389d7e3a2431c4c979db2
Phill Barron <phillipbarron@users.noreply.github.com>
2023-08-17 19:36:42 +0100

    make async workflow properties required (#234)

--- libs/newsletters-data-client/src/lib/newsletter-data-type.ts
+++ libs/newsletters-data-client/src/lib/newsletter-data-type.ts
@@ -199,10 +199,10 @@ export const newsletterDataSchema = z.object({
 	renderingOptions: renderingOptionsSchema.optional(),
 	thrasherOptions: thrasherOptionsSchema.optional(),
 	mailSuccessDescription: z.string().optional(),
-	brazeCampaignCreationsStatus: workflowStatusEnumSchema.optional(),
-	ophanCampaignCreationsStatus: workflowStatusEnumSchema.optional(),
-	signupPageCreationsStatus: workflowStatusEnumSchema.optional(),
-	tagCreationsStatus: workflowStatusEnumSchema.optional(),
+	brazeCampaignCreationsStatus: workflowStatusEnumSchema,
+	ophanCampaignCreationsStatus: workflowStatusEnumSchema,
+	signupPageCreationsStatus: workflowStatusEnumSchema,
+	tagCreationsStatus: workflowStatusEnumSchema,
 });
 
 /** NOT FINAL - this type a placeholder to test the data transformation structure */
