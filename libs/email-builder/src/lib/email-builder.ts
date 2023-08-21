e5c7f4d29b6e873801b985b3b59f83c060c62eb2
David Blatcher <dv.bltchr@gmail.com>
2023-08-21 14:39:56 +0100

    getMessageConfig function

--- libs/email-builder/src/lib/email-builder.ts
+++ libs/email-builder/src/lib/email-builder.ts
@@ -1,34 +1,62 @@
 import { SendEmailCommand } from '@aws-sdk/client-ses';
 import type { EmailEnvInfo } from '@newsletters-nx/newsletters-data-client';
 
-const getRecipients = (emailEnvInfo: EmailEnvInfo): string[] => {
-	const { STAGE, TEST_EMAIL_RECIPIENT } = emailEnvInfo;
-	const devMailingList = TEST_EMAIL_RECIPIENT ? [TEST_EMAIL_RECIPIENT] : [];
+type MessageConfig = {
+	/** the emails addresses to send the message to */
+	recipients: string[];
+	/** the URL for the version of the tool to point recipients to */
+	toolHost: string;
+	/** the sender name  and email address to use in the message header*/
+	source: string;
+};
 
-	return STAGE === 'DEV' ? devMailingList : ['newsletters.dev@guardian.co.uk'];
-	// Still testing - using newsletters.dev@guardian.co.uk as the 'live' recipient
-	// could use config for this too
+const getMessageConfig = (
+	prodRecipients: string[],
+	emailEnvInfo: EmailEnvInfo,
+): MessageConfig => {
+	const { STAGE, TEST_EMAIL_RECIPIENT } = emailEnvInfo;
+	// TO DO - should the recipients for CODE be done differently?
+	const testMailingList = TEST_EMAIL_RECIPIENT ? [TEST_EMAIL_RECIPIENT] : [];
+
+	switch (STAGE) {
+		case 'DEV':
+			return {
+				recipients: testMailingList,
+				toolHost: 'http://localhost:4200',
+				source:
+					'newsletters DEV <notifications@newsletters-tool.gutools.co.uk>',
+			};
+		case 'PROD':
+			return {
+				recipients: prodRecipients,
+				toolHost: 'https://newsletters-tool.gutools.co.uk',
+				source:
+					'newsletters <notifications@newsletters-tool.code.dev-gutools.co.uk>',
+			};
+		case 'CODE':
+		default:
+			return {
+				recipients: testMailingList,
+				toolHost: 'https://newsletters-tool.code.dev-gutools.co.uk',
+				source:
+					'newsletters CODE <notifications@newsletters-tool.gutools.co.uk>',
+			};
+	}
 };
 
 export function buildTestMessage(
 	newsletterId: string,
 	emailEnvInfo: EmailEnvInfo,
 ): SendEmailCommand {
-	const { STAGE } = emailEnvInfo;
+	const { recipients, source, toolHost } = getMessageConfig(
+		['newsletters.dev@guardian.co.uk'],
+		emailEnvInfo,
+	);
 
-	const updateLink = `${
-		STAGE === 'PROD'
-			? 'https://newsletters-tool.gutools.co.uk'
-			: 'https://newsletters-tool.code.dev-gutools.co.uk'
-	}/launched/edit/${newsletterId}`;
-
-	const recipients = getRecipients(emailEnvInfo);
+	const updateLink = `${toolHost}/launched/edit/${newsletterId}`;
 
 	return new SendEmailCommand({
-		Source:
-			STAGE !== 'PROD'
-				? 'newsletters <notifications@newsletters-tool.code.dev-gutools.co.uk>'
-				: 'newsletters CODE <notifications@newsletters-tool.gutools.co.uk>',
+		Source: source,
 		Destination: {
 			ToAddresses: recipients,
 		},
