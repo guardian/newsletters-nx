36ed55cabea222b1530a51d0f71d0787e422a961
David Blatcher <dv.bltchr@gmail.com>
2023-09-06 16:48:10 +0100

    add test for the newsletter-launched message

---
+++ libs/email-builder/src/lib/messages/newsletter-launched-message.spec.ts
@@ -0,0 +1,49 @@
+import type { NewsletterData } from '@newsletters-nx/newsletters-data-client';
+import {
+	buildNewsLetterLaunchMessage,
+	newsletterLaunchedRecipients,
+} from './newsletter-launched-message';
+
+const TEST_RECIPIENTS = ['test@example.com', 'example@test.net'];
+
+const testNewsletter: NewsletterData = {
+	name: 'Spec Email',
+} as NewsletterData;
+
+describe('buildTestEmail', () => {
+	it('should generate content and config, using test recipients when stage is not PROD', () => {
+		const output = buildNewsLetterLaunchMessage(
+			{
+				messageTemplateId: 'NEWSLETTER_LAUNCH',
+				newsletter: testNewsletter,
+			},
+			{
+				testRecipients: TEST_RECIPIENTS,
+				STAGE: 'DEV',
+				areEmailNotificationsEnabled: true,
+			},
+		);
+
+		expect(output.content.subject.includes(testNewsletter.name)).toBeTruthy();
+		expect(output.messageConfig.recipients).toEqual(TEST_RECIPIENTS);
+	});
+
+	it('should generate content and config, sending to the recipients defined for the template on PROD', () => {
+		const output = buildNewsLetterLaunchMessage(
+			{
+				messageTemplateId: 'NEWSLETTER_LAUNCH',
+				newsletter: testNewsletter,
+			},
+			{
+				testRecipients: TEST_RECIPIENTS,
+				STAGE: 'PROD',
+				areEmailNotificationsEnabled: true,
+			},
+		);
+
+		expect(output.content.subject.includes(testNewsletter.name)).toBeTruthy();
+		expect(output.messageConfig.recipients).toEqual(
+			newsletterLaunchedRecipients,
+		);
+	});
+});
--- libs/email-builder/src/lib/messages/newsletter-launched-message.ts
+++ libs/email-builder/src/lib/messages/newsletter-launched-message.ts
@@ -3,16 +3,15 @@ import { renderNewLaunchMessage } from '../components/NewLaunchMessage';
 import { getMessageConfig } from '../message-config';
 import type { MessageAboutNewsletterParams, MessageContent } from '../types';
 
+const recipients = ['newsletters.dev@guardian.co.uk'];
+export { recipients as newsletterLaunchedRecipients };
+
 export function buildNewsLetterLaunchMessage(
 	params: MessageAboutNewsletterParams,
 	emailEnvInfo: EmailEnvInfo,
 ) {
 	const { newsletter } = params;
-	const messageConfig = getMessageConfig(
-		['newsletters.dev@guardian.co.uk'],
-		emailEnvInfo,
-	);
-
+	const messageConfig = getMessageConfig(recipients, emailEnvInfo);
 	const pageLink = `${messageConfig.toolHost}/launched/${newsletter.identityName}`;
 
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-call -- render the component
