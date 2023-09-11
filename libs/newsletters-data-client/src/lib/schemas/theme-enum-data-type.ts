90923d7d18b216e164272a1e86ae6cd6270ad592
David Blatcher <dv.bltchr@gmail.com>
2023-09-11 11:11:29 +0100

    set the description on the theme enum to 'pillar' , and update description of th formSchema

--- libs/newsletters-data-client/src/lib/schemas/theme-enum-data-type.ts
+++ libs/newsletters-data-client/src/lib/schemas/theme-enum-data-type.ts
@@ -1,12 +1,7 @@
 import { z } from 'zod';
 
-export const themeEnumSchema = z.enum([
-	'news',
-	'opinion',
-	'culture',
-	'sport',
-	'lifestyle',
-	'features',
-]);
+export const themeEnumSchema = z
+	.enum(['news', 'opinion', 'culture', 'sport', 'lifestyle', 'features'])
+	.describe('pillar');
 
 export type Theme = z.infer<typeof themeEnumSchema>;
