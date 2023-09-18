728a60a036c7c9ed644c2f85412d53fa812415f5
Phill Barron <phillipbarron242@gmail.com>
2023-09-18 09:50:16 +0100

    Bump padding to allow some space for status badge

--- apps/newsletters-ui/src/app/ContentWrapper.tsx
+++ apps/newsletters-ui/src/app/ContentWrapper.tsx
@@ -9,7 +9,7 @@ export const ContentWrapper = ({
 	maxWidth?: ContainerProps['maxWidth'];
 }) => (
 	<Container maxWidth={maxWidth}>
-		<Grid container spacing={2} rowSpacing={2} paddingY={2}>
+		<Grid container spacing={2} rowSpacing={2} paddingY={3}>
 			<Grid item container xs={12} display="flex" direction="column">
 				{children}
 			</Grid>
