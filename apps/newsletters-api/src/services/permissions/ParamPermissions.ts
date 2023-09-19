a66a373c0e0546afb65c9da86113b1888c11d039
David Blatcher <dv.bltchr@gmail.com>
2023-09-19 15:46:41 +0100

    set 15minute max age on permission data

--- apps/newsletters-api/src/services/permissions/ParamPermissions.ts
+++ apps/newsletters-api/src/services/permissions/ParamPermissions.ts
@@ -7,13 +7,20 @@ import {
 import { getConfigValue } from '@newsletters-nx/util';
 import type { PermissionsService } from './abstract-class';
 
+/** 15 minutes*/
+const TIME_BETWEEN_PERMISSIONS_PARAM_CHECKS = 1000 * 60 * 15;
+
 const getPermissionsData = async (): Promise<
 	Partial<Record<string, UserAccessLevel>>
 > => {
 	try {
-		const value = await getConfigValue('userPermissions', { maxAge: 10000 });
-		const json = JSON.parse(value) as unknown;
-		return permissionsDataSchema.parse(json) as Record<string, UserAccessLevel>;
+		const value = await getConfigValue('userPermissions', {
+			maxAge: TIME_BETWEEN_PERMISSIONS_PARAM_CHECKS,
+		});
+		return permissionsDataSchema.parse(JSON.parse(value)) as Record<
+			string,
+			UserAccessLevel
+		>;
 	} catch (error) {
 		console.warn('getPermissionsData failed');
 		console.warn(error);
