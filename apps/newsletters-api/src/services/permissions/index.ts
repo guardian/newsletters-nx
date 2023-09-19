eeec5e9dc4f180be5fbd72811311d2134330fe58
David Blatcher <dv.bltchr@gmail.com>
2023-09-19 15:43:03 +0100

    use param store for permissions if configured

--- apps/newsletters-api/src/services/permissions/index.ts
+++ apps/newsletters-api/src/services/permissions/index.ts
@@ -1,5 +1,10 @@
+import { isUsingLocalUserPermissions } from '../../apiDeploymentSettings';
+import type { PermissionsService } from './abstract-class';
 import { LocalPermissionService } from './LocalPermissions';
+import { ParamPermissionService } from './ParamPermissions';
 
-const permissionService = new LocalPermissionService();
+const permissionService: PermissionsService = isUsingLocalUserPermissions()
+	? new LocalPermissionService()
+	: new ParamPermissionService();
 
 export { permissionService };
