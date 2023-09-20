3875848994d79ad80d8a94831164b75bae48ec10
David Blatcher <dv.bltchr@gmail.com>
2023-09-20 16:13:30 +0100

    sort linting compliants on config

--- apps/newsletters-ui-e2e/cypress.config.ts
+++ apps/newsletters-ui-e2e/cypress.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
 
+// eslint-disable-next-line import/no-default-export -- config file
 export default defineConfig({
 	e2e: nxE2EPreset(__dirname, {
 		bundler: 'vite',
