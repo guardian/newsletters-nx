fc5643aa45404330faaee0d5138ca31e03fe8a2b
David Blatcher <dv.bltchr@gmail.com>
2023-09-21 13:52:44 +0100

    can use the NavigateButton

--- apps/newsletters-ui/src/app/components/MainNav.tsx
+++ apps/newsletters-ui/src/app/components/MainNav.tsx
@@ -70,7 +70,7 @@ export function MainNav({ isOnCode }: Props) {
 		<AppBar position="fixed" component={'header'}>
 			<Container maxWidth="xl">
 				<Toolbar disableGutters>
-					<Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+					<Box sx={{ display: { xs: 'flex', md: 'none' } }}>
 						<IconButton
 							size="large"
 							aria-label="account of current user"
--- apps/newsletters-ui/src/app/components/NewslettersBrandHeading.tsx
+++ apps/newsletters-ui/src/app/components/NewslettersBrandHeading.tsx
@@ -1,47 +1,39 @@
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import { Box, Typography } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import { Typography } from '@mui/material';
+import { NavigateButton } from './NavigateButton';
 
 export const NewslettersBrandHeading = () => {
-	const navigate = useNavigate();
-
 	return (
-		<>
+		<NavigateButton
+			href="/"
+			sx={{
+				cursor: 'pointer',
+				margin: 0,
+				flexGrow: {
+					xs: 1,
+					md: 0,
+				},
+				color: 'inherit',
+				textDecoration: 'none',
+				textTransform: 'none',
+				padding: 0,
+				minWidth: 'unset',
+			}}
+		>
 			<MailOutlineIcon sx={{ display: 'flex', mr: 1 }} />
-			<Box
-				role={'link'}
-				onClick={(event) => {
-					// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call -- is a sythetic pointer event
-					event.preventDefault();
-					navigate('/');
-				}}
+			<Typography
+				variant="h1"
+				noWrap
 				sx={{
-					cursor: 'pointer',
 					margin: 0,
-					flexGrow: {
-						xs: 1,
-						md: 0,
-					},
-					color: 'inherit',
-					textDecoration: 'none',
+					mr: 2,
+					display: 'flex',
+					fontSize: { xs: '1.5rem', md: '1.25rem' },
+					lineHeight: { xs: '1.334', md: '1.6' },
 				}}
-				href="#"
-				component={'a'}
 			>
-				<Typography
-					variant="h1"
-					noWrap
-					sx={{
-						margin: 0,
-						mr: 2,
-						display: 'flex',
-						fontSize: { xs: '1.5rem', md: '1.25rem' },
-						lineHeight: { xs: '1.334', md: '1.6' },
-					}}
-				>
-					Newsletters
-				</Typography>
-			</Box>
-		</>
+				Newsletters
+			</Typography>
+		</NavigateButton>
 	);
 };
