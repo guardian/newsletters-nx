a3c902b4e06b14feb010338b9850500bc242be40
Phill Barron <phillipbarron242@gmail.com>
2023-09-25 14:57:48 +0100

    Email formatting tweaks

--- libs/email-builder/src/lib/messages/index.ts
+++ libs/email-builder/src/lib/messages/index.ts
@@ -2,4 +2,3 @@ export * from './new-draft-message';
 export * from './newsletter-launched-message';
 export * from './request-braze-setup-message';
 export * from './request-tags-and-signup-page-message';
-
