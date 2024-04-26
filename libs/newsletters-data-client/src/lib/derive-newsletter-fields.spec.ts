6a689acabde2c6b532945d242b1cf4139bfd81f1
Phill Barron <phillipbarron242@gmail.com>
2024-04-26 08:15:20 +0100

    remove test redundancy

--- libs/newsletters-data-client/src/lib/derive-newsletter-fields.spec.ts
+++ libs/newsletters-data-client/src/lib/derive-newsletter-fields.spec.ts
@@ -2,7 +2,7 @@ import { deriveNewsletterFieldsFromName } from './derive-newsletter-fields';
 
 describe('deriveNewsletterFieldsFromName', () => {
 	it('should derive the newsletter fields', () => {
-		expect(deriveNewsletterFieldsFromName("foo bar")).toEqual({
+		const expectedOutput = {
 			identityName: "foo-bar",
 			brazeSubscribeEventNamePrefix: "foo_bar",
 			brazeNewsletterName: "Editorial_foobar",
@@ -10,26 +10,9 @@ describe('deriveNewsletterFieldsFromName', () => {
 			brazeSubscribeAttributeNameAlternate: ["email_subscribe_foo_bar"],
 			campaignName: "foobar",
 			campaignCode: "foobar_email",
-		});
-
-		expect(deriveNewsletterFieldsFromName("foo - bar")).toEqual({
-			identityName: "foo-bar",
-			brazeSubscribeEventNamePrefix: "foo_bar",
-			brazeNewsletterName: "Editorial_foobar",
-			brazeSubscribeAttributeName: "foobar_Subscribe_Email",
-			brazeSubscribeAttributeNameAlternate: ["email_subscribe_foo_bar"],
-			campaignName: "foobar",
-			campaignCode: "foobar_email",
-		});
-	});
-
-	expect(deriveNewsletterFieldsFromName("foo - ::: ----- bar ---------")).toEqual({
-		identityName: "foo-bar",
-		brazeSubscribeEventNamePrefix: "foo_bar",
-		brazeNewsletterName: "Editorial_foobar",
-		brazeSubscribeAttributeName: "foobar_Subscribe_Email",
-		brazeSubscribeAttributeNameAlternate: ["email_subscribe_foo_bar"],
-		campaignName: "foobar",
-		campaignCode: "foobar_email",
+		};
+		expect(deriveNewsletterFieldsFromName("foo bar")).toEqual(expectedOutput);
+		expect(deriveNewsletterFieldsFromName("foo - bar")).toEqual(expectedOutput);
+		expect(deriveNewsletterFieldsFromName("foo - ::: ----- bar ---------")).toEqual(expectedOutput);
 	});
 });
