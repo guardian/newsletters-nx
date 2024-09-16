b05e9e8eb1227bdcb2b5dbf9579711194f02cdc7
David Blatcher <dv.bltchr@gmail.com>
2024-09-16 17:54:37 +0100

    layout page has json editor for layouts

--- apps/newsletters-ui/src/app/components/views/LayoutView.tsx
+++ apps/newsletters-ui/src/app/components/views/LayoutView.tsx
@@ -1,17 +1,23 @@
 import { Typography } from '@mui/material';
-import { useLoaderData, useLocation } from 'react-router-dom';
 import {
 	editionIds,
+	layoutSchema,
 	type Layout,
 	type NewsletterData,
 } from '@newsletters-nx/newsletters-data-client';
+import { useState } from 'react';
+import { useLoaderData, useLocation } from 'react-router-dom';
+import { fetchPostApiData } from '../../api-requests/fetch-api-data';
 import { ContentWrapper } from '../../ContentWrapper';
+import { JsonEditor } from '../JsonEditor';
 import { LayoutDisplay } from '../LayoutDisplay';
 
 export const LayoutView = () => {
 	const data = useLoaderData() as
 		| { layout: Layout; newsletters: NewsletterData[] }
 		| undefined;
+
+	const [localLayout, setLocalLayout] = useState(data?.layout);
 
 	const location = useLocation();
 	const editionId = location.pathname.split('/').pop()?.toUpperCase();
@@ -30,12 +36,35 @@ export const LayoutView = () => {
 		);
 	}
 
-	const { layout, newsletters } = data;
+	const { layout: originalLayout, newsletters } = data;
+
+	const handleUpdate = async (updatedLayout: Layout) => {
+		console.log(updatedLayout);
+
+		const result = await fetchPostApiData(
+			`/api/layouts/${editionId}`,
+			updatedLayout,
+		);
+		if (result) {
+			console.log(result);
+			setLocalLayout(updatedLayout);
+		} else {
+			alert('failed to create layout');
+		}
+	};
 
 	return (
 		<ContentWrapper>
 			<Typography variant="h2">Layout for {editionId}</Typography>
-			<LayoutDisplay newsletters={newsletters} layout={layout} />
+			<LayoutDisplay newsletters={newsletters} layout={localLayout ?? []} />
+
+			<JsonEditor
+				schema={layoutSchema}
+				originalData={originalLayout}
+				submit={(updatedLayout) => {
+					void handleUpdate(updatedLayout);
+				}}
+			/>
 		</ContentWrapper>
 	);
 };
