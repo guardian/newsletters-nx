e8ca86cda9e758ab27ce01b9e2d254c5a449048c
David Blatcher <dv.bltchr@gmail.com>
2024-10-25 12:25:43 +0100

    read and create work

--- libs/newsletters-data-client/src/lib/layout-storage/S3LayoutStorage.ts
+++ libs/newsletters-data-client/src/lib/layout-storage/S3LayoutStorage.ts
@@ -2,8 +2,8 @@ import type { S3Client } from '@aws-sdk/client-s3';
 import {
 	fetchObject,
 	getListOfObjectsKeys,
-	// objectExists,
-	// putObject,
+	objectExists,
+	putObject,
 } from '../generic-s3-functions';
 import type {
 	SuccessfulStorageResponse,
@@ -12,8 +12,12 @@ import type {
 import { StorageRequestFailureReason } from '../storage-response-types';
 import type { LayoutStorage } from './LayoutStorage';
 import { objectToLayout } from './objectToLayout';
-import type { EditionId, EditionsLayouts, Layout } from './types';
-// import { layoutSchema } from './types';
+import {
+	editionIdSchema,
+	type EditionId,
+	type EditionsLayouts,
+	type Layout,
+} from './types';
 
 export class S3LayoutStorage implements LayoutStorage {
 	readonly s3Client: S3Client;
@@ -25,34 +29,95 @@ export class S3LayoutStorage implements LayoutStorage {
 		this.s3Client = s3Client;
 	}
 
-	create(
+	async create(
 		edition: EditionId,
 		layout: Layout,
 	): Promise<SuccessfulStorageResponse<Layout> | UnsuccessfulStorageResponse> {
-		console.log('create', edition, layout);
-		throw new Error('Method not implemented.');
+		try {
+			const newIdentifier = `${edition}.json`;
+
+			try {
+				const layoutWithSameKeyExists = await this.objectExists(newIdentifier);
+				if (layoutWithSameKeyExists) {
+					return {
+						ok: false,
+						message: `Layout ${edition} already exists`,
+						reason: StorageRequestFailureReason.DataInStoreNotValid,
+					};
+				}
+			} catch (err) {
+				return {
+					ok: false,
+					message: `failed to check if layout for ${edition} exists`,
+					reason: StorageRequestFailureReason.S3Failure,
+				};
+			}
+
+			try {
+				await this.putObject(layout, newIdentifier);
+			} catch (err) {
+				return {
+					ok: false,
+					message: `failed create layout ${edition}.`,
+					reason: StorageRequestFailureReason.S3Failure,
+				};
+			}
+			return {
+				ok: true,
+				data: layout,
+			};
+		} catch (error) {
+			console.error(error);
+			return {
+				ok: false,
+				message: `failed to create newsletter ${edition}`,
+				reason: StorageRequestFailureReason.S3Failure,
+			};
+		}
 	}
-	read(
+
+	async read(
 		edition: EditionId,
 	): Promise<SuccessfulStorageResponse<Layout> | UnsuccessfulStorageResponse> {
 		console.log('read', edition);
-		throw new Error('Method not implemented.');
+
+		try {
+			const layout = await this.fetchLayout(edition);
+
+			if (!layout) {
+				return {
+					ok: false,
+					message: `failed to read layout with name '${edition}'`,
+				};
+			}
+
+			return {
+				ok: true,
+				data: layout,
+			};
+		} catch (error) {
+			console.error(error);
+			return {
+				ok: false,
+				message: `failed to read layout for ${edition}`,
+				reason: StorageRequestFailureReason.S3Failure,
+			};
+		}
 	}
 	async readAll(): Promise<
 		SuccessfulStorageResponse<EditionsLayouts> | UnsuccessfulStorageResponse
 	> {
 		try {
 			const listOfObjectsKeys = await this.getListOfObjectsKeys();
-			console.log(listOfObjectsKeys);
-
 			const layouts: EditionsLayouts = {};
-
 			await Promise.all(
 				listOfObjectsKeys.map(async (key) => {
-					console.log({ key });
+					const editionId = this.keyToEditionId(key);
 					const s3Response = await this.fetchObject(key);
 					const responseAsLayout = await objectToLayout(s3Response);
-					console.log({ key, responseAsLayout });
+					if (responseAsLayout && editionId) {
+						layouts[editionId] = responseAsLayout;
+					}
 				}),
 			);
 
@@ -83,8 +148,20 @@ export class S3LayoutStorage implements LayoutStorage {
 		throw new Error('Method not implemented.');
 	}
 
+	private async fetchLayout(edition: EditionId): Promise<Layout | undefined> {
+		const key = `${this.OBJECT_PREFIX}${edition}.json`;
+		const s3Object = await this.fetchObject(key);
+		return await objectToLayout(s3Object);
+	}
+
+	private keyToEditionId(key: string): EditionId | undefined {
+		// format = 'layouts/UK.json'
+		const unparsedEditionId = key.split('/').pop()?.split('.').shift();
+		return editionIdSchema.safeParse(unparsedEditionId).data;
+	}
+
 	private fetchObject = fetchObject(this);
-	// private putObject = putObject(this);
-	// private objectExists = objectExists(this);
+	private putObject = putObject(this);
+	private objectExists = objectExists(this);
 	private getListOfObjectsKeys = getListOfObjectsKeys(this);
 }
