62b2be70bff933eec3cf85b6463188844990a544
Sam Hession <sam.hession@guardian.co.uk>
2026-01-27 08:26:10 +0000

    Adds a telemetry tracking pixel to the page Layout

--- apps/newsletters-ui/src/app/Layout.tsx
+++ apps/newsletters-ui/src/app/Layout.tsx
@@ -1,4 +1,6 @@
 import { Box, css } from '@mui/material';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router';
 import { Outlet } from 'react-router-dom';
 import { MainNav } from './components/MainNav';
 
@@ -23,10 +25,24 @@ interface IRootRoute {
 export function Layout(props: IRootRoute) {
 	// Not ideal to use the host name to determine environment.
 	// Could also use a hook to query the API on a route that exposes the
-	// process.env.STAGE value, but taht seems unnecessary.
+	// process.env.STAGE value, but that seems unnecessary.
 	const host = typeof window !== 'undefined' ? window.location.host : undefined;
 	const isOnCode = !!host?.toLowerCase().split('.').includes('code');
 	const isOnLocal = !!host?.toLowerCase().split(':').includes('localhost');
+
+	const location = useLocation();
+
+	useEffect(() => {
+		let hostname = 'user-telemetry.gutools.co.uk';
+		if(isOnCode){
+			hostname = 'user-telemetry.code.dev-gutools.co.uk';
+		}
+		else if(isOnLocal){
+			hostname = 'user-telemetry.local.dev-gutools.co.uk';
+		}
+		const image = new Image();
+		image.src = `https://${hostname}/guardian-tool-accessed?app=newsletters-tool&path=${location.pathname}`;
+	}, [isOnCode, isOnLocal, location.pathname]);
 
 	return (
 		<div css={frameCss}>
