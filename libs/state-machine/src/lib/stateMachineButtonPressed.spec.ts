f84c34b1be6dcfa9389b0a70f60e79eff3d2f685
Richard Bangay <richard.bangay@guardian.co.uk>
2026-04-08 14:38:42 +0100

    migrate nx to v21

--- libs/state-machine/src/lib/stateMachineButtonPressed.spec.ts
+++ libs/state-machine/src/lib/stateMachineButtonPressed.spec.ts
@@ -98,7 +98,7 @@ describe('stateMachineButtonPressed', () => {
 				true,
 				mockStorage,
 			),
-		).rejects.toThrowError('Button poop not found in step step1');
+		).rejects.toThrow('Button poop not found in step step1');
 	});
 
 	it('should execute step and move to next step if next button is pressed', async () => {
