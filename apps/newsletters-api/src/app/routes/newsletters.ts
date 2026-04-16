89c7b6d6edd9d6a712a9ee8857fb20d8276e614b
Richard Bangay <richard.bangay@guardian.co.uk>
2026-04-16 11:54:20 +0100

    sort out funky rebase

--- apps/newsletters-api/src/app/routes/newsletters.ts
+++ apps/newsletters-api/src/app/routes/newsletters.ts
@@ -118,101 +118,6 @@ export function registerReadWriteNewsletterRoutes(app: Express) {
 			if (failedValidation) {
 				return;
 			}
-<<<<<<< HEAD
-			const user = getUserProfile(req);
-
-			const { newsletterId } = req.params;
-			const modifications = req.body as unknown;
-			const newsletterIdAsNumber = Number(newsletterId);
-
-			if (isNaN(newsletterIdAsNumber)) {
-				return res
-					.status(400)
-					.send(makeErrorResponse(`Non numeric id provided`));
-			}
-
-			replaceNullWithUndefinedForUnknown(modifications);
-
-			if (!isPartialNewsletterData(modifications)) {
-				return res
-					.status(400)
-					.send(makeErrorResponse(`Not a valid partial newsletter`));
-			}
-
-			// This test would never fail on the current implementation since
-			// user.profile must be defined or there would be an accessDeniedError.
-			// Kept in to preserve type-safety.
-			if (!user.profile) {
-				return res.status(403).send(makeErrorResponse('No user profile.'));
-			}
-			const storageResponse = await newsletterStore.update(
-				newsletterIdAsNumber,
-				modifications,
-				user.profile,
-			);
-
-			if (!storageResponse.ok) {
-				return res
-					.status(mapStorageFailureReasonToStatusCode(storageResponse.reason))
-					.send(makeErrorResponse(storageResponse.message));
-			}
-
-			return res.send(makeSuccessResponse(storageResponse.data));
-		},
-	);
-
-	app.post(
-		'/api/newsletters/:newsletterId',
-		writeNewsletterRateLimiter,
-		async (req, res) => {
-			const failedValidation = await doesNotHaveAccess(req, res);
-			if (failedValidation) {
-				return;
-			}
-			const user = getUserProfile(req);
-			const accessDeniedError = await makeAccessDeniedApiResponse(
-				user.profile,
-				'editNewsletters',
-			);
-			if (accessDeniedError) {
-				return res.status(403).send(accessDeniedError);
-			}
-
-			const { newsletterId } = req.params;
-			const newsletter = req.body as unknown;
-			const newsletterIdAsNumber = Number(newsletterId);
-
-			if (isNaN(newsletterIdAsNumber)) {
-				return res
-					.status(400)
-					.send(makeErrorResponse(`Non numeric id provided`));
-			}
-
-			replaceNullWithUndefinedForUnknown(newsletter);
-
-			if (!isNewsletterData(newsletter)) {
-				return res
-					.status(400)
-					.send(makeErrorResponse(`Not a valid newsletter`));
-			}
-
-			if (!user.profile) {
-				return res.status(403).send(makeErrorResponse('No user profile'));
-			}
-
-			const storageResponse = await newsletterStore.replace(
-				newsletterIdAsNumber,
-				newsletter,
-				user.profile,
-			);
-
-			if (!storageResponse.ok) {
-				return res
-					.status(mapStorageFailureReasonToStatusCode(storageResponse.reason))
-					.send(makeErrorResponse(storageResponse.message));
-			}
-
-=======
 
 			const user = getUserProfile(req);
 
@@ -308,7 +213,6 @@ export function registerReadWriteNewsletterRoutes(app: Express) {
 					.send(makeErrorResponse(storageResponse.message));
 			}
 
->>>>>>> cfd2890 (add express rate-limit middleware to two newsletters.ts endpoints to address github codeQL warnings as a result of running new prettier config across the codebase (apparently codeQL only reports on lines that appear in a pr diff))
 			return res.send(makeSuccessResponse(storageResponse.data));
 		},
 	);
