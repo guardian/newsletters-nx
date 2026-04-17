5accb5a3c978f95d912c483dea7b6294080fa5aa
Andrew Howe-Ely <Andrew.howe-ely@guardian.co.uk>
2026-04-17 14:40:06 +0100

    create new form flow for redesign

---
+++ libs/newsletter-workflow/src/lib/steps/redesignNewsletter/nameAndFrequencyLayout.ts
@@ -0,0 +1,32 @@
+import type { DraftService } from '@newsletters-nx/newsletters-data-client';
+import { getNextStepId } from '@newsletters-nx/state-machine';
+import type { WizardStepLayout } from '@newsletters-nx/state-machine';
+import { executeCreate } from '../../executeCreate';
+import { formSchemas } from './formSchemas';
+
+export const nameAndFrequencyLayout: WizardStepLayout<DraftService> = {
+	staticMarkdown: `# Enter the newsletter's name
+
+The first step is to enter the name of your newsletter. For example,  **Down to Earth**.
+
+`,
+	label: 'Name & frequency',
+	buttons: {
+		cancel: {
+			buttonType: 'CANCEL',
+			label: 'Cancel',
+			stepToMoveTo: 'cancel',
+		},
+		next: {
+			buttonType: 'NEXT',
+			label: 'Save and Continue',
+			stepToMoveTo: getNextStepId,
+			executeStep: executeCreate,
+		},
+	},
+	schema: formSchemas.nameAndFrequency,
+	canSkipTo: true,
+	skippingWillPersistLocalChanges: true,
+	role: 'CREATE_START',
+	executeSkip: executeCreate,
+};
