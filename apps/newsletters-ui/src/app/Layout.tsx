804eb4bb66f78f8fed2cbb6d2b2f350d04dd7b29
Andrew Howe-Ely <114918544+andrewHEguardian@users.noreply.github.com>
2026-04-21 11:53:08 +0100

    Revert "Revert "Implement a simple client-side feature switch functionality (…" (#605)
    This reverts commit af58012ce917cc3c791fd5b85d3f86ad0ed2f27f.

--- apps/newsletters-ui/src/app/Layout.tsx
+++ apps/newsletters-ui/src/app/Layout.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router';
 import { Outlet } from 'react-router-dom';
 import { MainNav } from './components/MainNav';
+import { isFeatureSwitchEnabled } from './featureSwitches';
 
 const frameCss = css`
 	display: flex;
@@ -32,6 +33,9 @@ export function Layout(props: IRootRoute) {
 		!!host?.toLowerCase().split(':').includes('localhost') ||
 		!!host?.toLowerCase().split('.').includes('local');
 
+	const isUsingStand =
+		isFeatureSwitchEnabled('switch-stand') && (isOnCode || isOnLocal);
+
 	const location = useLocation();
 
 	useEffect(() => {
@@ -49,6 +53,7 @@ export function Layout(props: IRootRoute) {
 		<div css={frameCss}>
 			<MainNav isOnCode={isOnCode} isOnLocal={isOnLocal} />
 			<Box sx={{ pt: 8 }} component={'main'}>
+				{isUsingStand && <>Stand Version</>}
 				{props.outlet ?? <Outlet />}
 			</Box>
 		</div>
