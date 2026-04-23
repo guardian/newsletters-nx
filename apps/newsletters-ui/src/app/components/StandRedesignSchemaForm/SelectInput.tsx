b5279c3a79ed340a9949e0b3c292b0c5d8d239af
Andrew Howe-Ely <Andrew.howe-ely@guardian.co.uk>
2026-04-23 09:42:19 +0100

    add description to rest of fields

--- apps/newsletters-ui/src/app/components/StandRedesignSchemaForm/SelectInput.tsx
+++ apps/newsletters-ui/src/app/components/StandRedesignSchemaForm/SelectInput.tsx
@@ -1,19 +1,26 @@
 import { Option, Select } from '@guardian/stand/select';
 import type { FunctionComponent } from 'react';
 import type { Key } from 'react-aria-components';
+import type { StandardFormProps } from './SchemaField';
 import type { FieldProps } from './util';
 
 const EMPTY_STRING = '';
 
 export const StandSelectInput: FunctionComponent<
-	FieldProps & {
-		value: string | undefined;
-		optional?: boolean;
-		inputHandler: { (value: string | undefined): void };
-		options: string[];
-	}
+	FieldProps &
+		StandardFormProps & {
+			value: string | undefined;
+			options: string[];
+		}
 > = (props) => {
-	const { value, optional, options, inputHandler, label = 'value' } = props;
+	const {
+		value,
+		description,
+		optional,
+		options,
+		inputHandler,
+		label = 'value',
+	} = props;
 	const handleChange = (value: Key | Key[] | null) => {
 		console.log(value);
 		if (value === EMPTY_STRING || value === null) {
@@ -24,7 +31,12 @@ export const StandSelectInput: FunctionComponent<
 	const valueAsString = value ?? EMPTY_STRING;
 
 	return (
-		<Select label={label} value={valueAsString} onChange={handleChange}>
+		<Select
+			label={label}
+			description={description}
+			value={valueAsString}
+			onChange={handleChange}
+		>
 			{optional && <Option value={{ EMPTY_STRING }}>[none]</Option>}
 			{options.map((option) => (
 				<Option value={{ option }}>{option}</Option>
