b04f4f43071add74de1e26e48e208f1c3707cb60
ben.dudiak-fry <ben.dudiak-fry@guardian.co.uk>
2026-09-18 11:58:04 +0100

    test(e2e): Add end-to-end tests for checking that you cannot skip steps in the create newsletter wizard when on the 'Review' or 'Finish' steps.

--- apps/newsletters-e2e/src/ui/steps/create-newsletter-wizard.steps.ts
+++ apps/newsletters-e2e/src/ui/steps/create-newsletter-wizard.steps.ts
@@ -174,3 +174,245 @@ When(
 			.fill('https://www.example.com/');
 	},
 );
+
+Given(
+	"the editor has completed up until the 'Review' step",
+	async ({ page, createDraftNewsletterWizard }) => {
+		const continueToNextStep = async () => {
+			await createDraftNewsletterWizard.gotoNextStep();
+		};
+
+		await continueToNextStep();
+
+		await page.getByLabel('Name the newsletter').fill('example');
+		await page.getByLabel('Set the frequency').getByText('Monthly').check();
+		await continueToNextStep();
+
+		await page
+			.getByLabel('Type of newsletter')
+			.getByText('article-based')
+			.check();
+		await page
+			.getByLabel('Location of newsletter')
+			.getByText('Web for first send only')
+			.check();
+		await continueToNextStep();
+
+		const fillDateSegment = async (
+			group: Locator,
+			name: string,
+			value: string,
+		) => {
+			await group.getByRole('spinbutton', { name }).click();
+			await page.keyboard.type(value);
+		};
+
+		const launchDateGroup = page.getByRole('group', {
+			name: 'Enter launch date',
+		});
+		await fillDateSegment(launchDateGroup, 'day', '09');
+		await fillDateSegment(launchDateGroup, 'month', '12');
+		await fillDateSegment(launchDateGroup, 'year', '2027');
+
+		const signUpDateGroup = page.getByRole('group', {
+			name: 'Enter sign up page date',
+		});
+		await fillDateSegment(signUpDateGroup, 'day', '18');
+		await fillDateSegment(signUpDateGroup, 'month', '12');
+		await fillDateSegment(signUpDateGroup, 'year', '2027');
+		await continueToNextStep();
+
+		await page
+			.getByRole('radiogroup', { name: 'Region focus' })
+			.getByText('UK')
+			.check();
+
+		const selectOption = async (label: string, option: string) => {
+			await page.getByLabel(label).click();
+			await page
+				.getByRole('listbox')
+				.getByRole('option', { name: option })
+				.click();
+		};
+
+		await selectOption('Pillar', 'sport');
+		await selectOption('Group for MMA page', 'Opinion');
+		await continueToNextStep();
+
+		await page
+			.getByRole('textbox', {
+				name: 'Add the series tag',
+				exact: true,
+			})
+			.fill('example/series');
+		await page
+			.getByRole('textbox', {
+				name: 'Add the Series tag description',
+				exact: true,
+			})
+			.fill('Example series tag description');
+		await page
+			.getByRole('textbox', {
+				name: 'Campaign tag',
+				exact: true,
+			})
+			.fill('Example (newsletter sign up)');
+		await page
+			.getByRole('textbox', {
+				name: 'Campaign description',
+				exact: true,
+			})
+			.fill('Example campaign tag description');
+		await continueToNextStep();
+
+		await page.getByLabel('Headline').fill('Example headline');
+		await page
+			.getByLabel('Description', { exact: true })
+			.fill('Example description');
+		await page
+			.getByLabel('Embed description')
+			.fill('Example embed description');
+		await page
+			.getByLabel('Success message', { exact: true })
+			.fill('Example success message. Hurray!');
+		await page
+			.getByLabel('Highlight card message', { exact: true })
+			.fill('Example message for highlight card.');
+		await page
+			.getByPlaceholder('URL of the newsletter graphic 5:4')
+			.fill('https://www.example.com/');
+		await page
+			.getByPlaceholder('URL of the newsletter graphic 1:1')
+			.fill('https://www.example.com/');
+		await continueToNextStep();
+	},
+);
+
+Given(
+	"the editor has completed up until the 'Finish' step",
+	async ({ page, createDraftNewsletterWizard }) => {
+		const continueToNextStep = async () => {
+			await createDraftNewsletterWizard.gotoNextStep();
+		};
+
+		await continueToNextStep();
+
+		await page.getByLabel('Name the newsletter').fill('example');
+		await page.getByLabel('Set the frequency').getByText('Monthly').check();
+		await continueToNextStep();
+
+		await page
+			.getByLabel('Type of newsletter')
+			.getByText('article-based')
+			.check();
+		await page
+			.getByLabel('Location of newsletter')
+			.getByText('Web for first send only')
+			.check();
+		await continueToNextStep();
+
+		const fillDateSegment = async (
+			group: Locator,
+			name: string,
+			value: string,
+		) => {
+			await group.getByRole('spinbutton', { name }).click();
+			await page.keyboard.type(value);
+		};
+
+		const launchDateGroup = page.getByRole('group', {
+			name: 'Enter launch date',
+		});
+		await fillDateSegment(launchDateGroup, 'day', '09');
+		await fillDateSegment(launchDateGroup, 'month', '12');
+		await fillDateSegment(launchDateGroup, 'year', '2027');
+
+		const signUpDateGroup = page.getByRole('group', {
+			name: 'Enter sign up page date',
+		});
+		await fillDateSegment(signUpDateGroup, 'day', '18');
+		await fillDateSegment(signUpDateGroup, 'month', '12');
+		await fillDateSegment(signUpDateGroup, 'year', '2027');
+		await continueToNextStep();
+
+		await page
+			.getByRole('radiogroup', { name: 'Region focus' })
+			.getByText('UK')
+			.check();
+
+		const selectOption = async (label: string, option: string) => {
+			await page.getByLabel(label).click();
+			await page
+				.getByRole('listbox')
+				.getByRole('option', { name: option })
+				.click();
+		};
+
+		await selectOption('Pillar', 'sport');
+		await selectOption('Group for MMA page', 'Opinion');
+		await continueToNextStep();
+
+		await page
+			.getByRole('textbox', {
+				name: 'Add the series tag',
+				exact: true,
+			})
+			.fill('example/series');
+		await page
+			.getByRole('textbox', {
+				name: 'Add the Series tag description',
+				exact: true,
+			})
+			.fill('Example series tag description');
+		await page
+			.getByRole('textbox', {
+				name: 'Campaign tag',
+				exact: true,
+			})
+			.fill('Example (newsletter sign up)');
+		await page
+			.getByRole('textbox', {
+				name: 'Campaign description',
+				exact: true,
+			})
+			.fill('Example campaign tag description');
+		await continueToNextStep();
+
+		await page.getByLabel('Headline').fill('Example headline');
+		await page
+			.getByLabel('Description', { exact: true })
+			.fill('Example description');
+		await page
+			.getByLabel('Embed description')
+			.fill('Example embed description');
+		await page
+			.getByLabel('Success message', { exact: true })
+			.fill('Example success message. Hurray!');
+		await page
+			.getByLabel('Highlight card message', { exact: true })
+			.fill('Example message for highlight card.');
+		await page
+			.getByPlaceholder('URL of the newsletter graphic 5:4')
+			.fill('https://www.example.com/');
+		await page
+			.getByPlaceholder('URL of the newsletter graphic 1:1')
+			.fill('https://www.example.com/');
+		await continueToNextStep();
+
+		// Go to Finish step
+		await continueToNextStep();
+	},
+);
+
+Then('the wizard navigation is disabled', async ({ page }) => {
+	const allNavButtons = await page
+		.getByRole('navigation', { name: 'Newsletter creation steps' })
+		.getByRole('button')
+		.all();
+
+	for (const btn of allNavButtons) {
+		const isCurrent = (await btn.getAttribute('aria-current')) == 'step';
+		const isDisabled = await btn.isDisabled();
+		expect(isCurrent || isDisabled).toBe(true);
+	}
+});
